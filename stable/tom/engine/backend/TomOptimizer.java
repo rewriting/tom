df8602a27db21036eba0fdcf71c4a802a61c2d76
Pierre-Etienne Moreau <Pierre-Etienne.Moreau@loria.fr>
2003-03-19 14:42:45 +0000

    * forget

---
+++ stable/tom/engine/backend/TomOptimizer.java
@@ -0,0 +1,51 @@
+ /*
+  
+    TOM - To One Matching Compiler
+
+    Copyright (C) 2000-2003  LORIA (CNRS, INPL, INRIA, UHP, U-Nancy 2)
+			     Nancy, France.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+    Pierre-Etienne Moreau	e-mail: Pierre-Etienne.Moreau@loria.fr
+
+*/
+
+package jtom.backend;
+  
+import java.util.*;
+import java.io.*;
+
+import aterm.*;
+import aterm.pure.*;
+
+import jtom.*;
+import jtom.tools.*;
+import jtom.adt.*;
+
+public class TomOptimizer extends TomBase {
+
+  public TomOptimizer(jtom.TomEnvironment environment) {
+    super(environment);
+  }
+
+// ------------------------------------------------------------
+                                                                                                                                                                
+// ------------------------------------------------------------
+
+  public TomTerm optimize(TomTerm subject) {
+    return subject;
+  }
+} //Class 
