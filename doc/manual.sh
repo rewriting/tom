81e5a76a9baf5f5d5af7564ba2d13463d65c4bbc
Julien Guyon <Julien.Guyon@loria.fr>
2003-04-18 07:07:19 +0000

    * Added shell script for manual generation: ./manuel [-html | -dvips | all]

---
+++ doc/manual.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+    
+#default is to generate all
+
+usage () {
+        echo "Usage: $name [ -html (HTML cersion only) | -dvips (DVI and PS only) | -all ] " 1>&2
+	exit
+}
+
+if [ $# -ge 1 ]; then
+    for x in "$@"; do
+    case "$x" in
+    -html)
+	hevea -fix -o manual1.html manual.hva manual.tex
+	hevea -o manual1.html manual.hva manual.tex
+	hacha -o manual.html manual1.html
+	shift
+	;;
+    -dvips) 
+	shift
+	latex manual.tex
+	dvips manual.dvi >! manual.ps
+	;;
+    -all)
+	hevea -fix -o manual1.html manual.hva manual.tex
+	hevea -o manual1.html manual.hva manual.tex
+	hacha -o manual.html manual1.html
+	latex manual.tex
+	dvips manual.dvi >! manual.ps
+        ;;
+    -*)
+	usage
+        ;;    
+    esac
+    done
+else
+    hevea -fix -o manual1.html manual.hva manual.tex
+    hevea -o manual1.html manual.hva manual.tex
+    hacha -o manual.html manual1.html
+    latex manual.tex
+    dvips manual.dvi >! manual.ps
+fi
