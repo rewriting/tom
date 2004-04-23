587ca53f1e3be027fbd47a64df2f620dd1dd19dd
Pierre-Etienne Moreau <Pierre-Etienne.Moreau@loria.fr>
2004-04-23 13:35:00 +0000

    * version 2.0rc1 in preparation

--- src/tom/engine/tools/ant/TomTask.java
+++ src/tom/engine/tools/ant/TomTask.java
@@ -1,19 +1,29 @@
 /*
- * Copyright  2000-2004 The Apache Software Foundation
+ *   
+ * TOM - To One Matching Compiler
+ * 
+ * Copyright (C) 2004 INRIA
+ * Nancy, France.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *     
+ * Pierre-Etienne Moreau	e-mail: Pierre-Etienne.Moreau@loria.fr
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- */
+ **/
+
+package jtom.tools.ant;
 
 import java.io.File;
 import java.util.Vector;
@@ -62,14 +72,14 @@ import org.apache.tools.ant.util.SourceFileScanner;
  * @author Stefan Bodewig
  * @author <a href="mailto:jayglanville@home.com">J D Glanville</a>
  *
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.1 $
  *
  * @since Ant 1.1
  *
  * @ant.task category="java"
  */
 
-public class TomAntTask extends MatchingTask {
+public class TomTask extends MatchingTask {
 
   private static final String FAIL_MSG
   = "Compile failed; see the compiler error output for details.";
