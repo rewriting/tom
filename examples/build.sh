c36551f6723c4262de08aad5853f09517431f573
Antoine Reilles <Antoine.Reilles@loria.fr>
2004-04-24 07:36:39 +0000

    * trying to fix the cygwin problem

--- examples/build.sh
+++ examples/build.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# OS specific support.  $var _must_ be set to either true or false.
+cygwin=false;
+darwin=false;
+case "`uname`" in
+  CYGWIN*) cygwin=true ;;
+  Darwin*) darwin=true ;;
+esac
+
+# For Cygwin, ensure paths are in UNIX format before anything is touched
+if $cygwin ; then
+  [ -n "$TOM_HOME" ] &&
+    TOM_HOME=`cygpath --unix "$TOM_HOME"`
+fi
+
 LOCALCLASSPATH=`echo lib/junit.jar | tr ' ' ':'` 
 
 CLASSPATH=${LOCALCLASSPATH} ant $*
