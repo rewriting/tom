1122b3a77b82eef4ed3f7f6d01c92593099d33e1
Antoine Reilles <Antoine.Reilles@loria.fr>
2004-04-30 20:27:47 +0000

    * added two small tests

---
+++ examples/builtin/TestJstring.java
@@ -0,0 +1,32 @@
+package Builtin;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class TestJstring extends TestCase {
+	private Jstring test;
+
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(new TestSuite(TestJstring.class));
+  }
+
+  public void setUp() {
+    test = new Jstring();
+  }
+	
+	public void testFcoucou() {
+		assertEquals("input coucou gets coucou",
+								 "coucou",test.f("coucou"));
+	}
+
+	public void testFhello() {
+		assertEquals("input hello gets hello",
+								 "hello",test.f("hello"));
+	}
+
+	public void testFelse() {
+		assertEquals("input something gets unknown",
+								 "unknown",test.f("something"));
+	}
+
+}
