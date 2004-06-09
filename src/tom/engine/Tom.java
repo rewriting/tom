98da1dcc25f950f3fedb4a27c87bdfa394c46be7
Pierre-Etienne Moreau <Pierre-Etienne.Moreau@loria.fr>
2004-06-09 07:53:09 +0000

    * bootinstall * bumped to 2.0 * Tom 2.0 released

--- src/tom/engine/Tom.java
+++ src/tom/engine/Tom.java
@@ -79,7 +79,7 @@ public class Tom {
      */
   private List inputFileList;
   
-  public final static String VERSION = "2.0rc5";
+  public final static String VERSION = "2.0";
   
   public static String usage =
     "\nTom usage:"
