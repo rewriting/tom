75fe7b632036938419ec12dbefaf41e0fe42b29d
Pierre-Etienne Moreau <Pierre-Etienne.Moreau@loria.fr>
2004-08-12 13:32:22 +0000

    * fixed build.xml: build all should be OK

---
+++ stable/tom/platform/OptionManager.java
@@ -0,0 +1,75 @@
+package tom.platform;
+
+import java.util.*;
+import tom.library.adt.tnode.types.TNode;
+
+/**
+ *
+ *
+ * @author Gr&eacute;gory ANDRIEN
+ */
+public interface OptionManager {
+
+  /**
+   *
+   *
+   * @param plugins
+   * @return
+   */
+  public abstract void setPlugins(List plugins);
+
+  /**
+   *
+   *
+   * @param args
+   * @return
+   */
+  public abstract String[] optionManagement(String[] args);
+
+  /**
+   *
+   *
+   * @param optionName the option's name
+   * @return the option's value as an Object
+   */
+  public abstract Object getOptionValue(String optionName);
+
+  /**
+   *
+   *
+   * @param optionName the option's name
+   * @return the option's value as a boolean
+   */
+  public abstract boolean getOptionBooleanValue(String optionName);
+
+  /**
+   *
+   *
+   * @param optionName the option's name
+   * @return the option's value as an int
+   */
+  public abstract int getOptionIntegerValue(String optionName);
+
+  /**
+   *
+   *
+   * @param optionName the option's name
+   * @return the option's value as a String
+   */
+  public abstract String getOptionStringValue(String optionName);
+
+  /**
+   *
+   *
+   * @param key the option's name
+   * @param value the option's value
+   */
+  public abstract void putOptionValue(Object key, Object value);
+
+  /**
+   *
+   *
+   * @param xmlDocument
+   */
+  public void extractOptionList(TNode xmlDocument);
+}
