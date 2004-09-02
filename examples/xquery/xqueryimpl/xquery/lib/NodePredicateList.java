dd6ed0853bcc3205f6c86dc68b6aa51a5b9ffe8c
Nghiem Long Phan <phan@loria.fr>
2004-09-02 12:24:44 +0000

    cvs handling error

---
+++ examples/xquery/xqueryimpl/xquery/lib/NodePredicateList.java
@@ -0,0 +1,23 @@
+
+package xquery.lib; 
+
+import java.util.Vector;
+
+public class NodePredicateList extends Vector								   
+{
+  public NodePredicateList() 
+  {
+	super();
+  }
+}
+
+
+
+// Duong Thanh Hai
+// c/o Eagle College Suisse
+// Lincolnweg 23, P.O. Box 149
+// CH-8840. Einsiedeln
+// Switzerland.
+
+
+// 41 55 418 78 30
