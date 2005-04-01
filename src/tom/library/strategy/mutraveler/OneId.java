3554d0720ecbd7fb4476802433c56bbd5d0d2080
Antoine Reilles <Antoine.Reilles@loria.fr>
2005-04-01 14:21:19 +0000

    * add more Id strategies

---
+++ src/tom/library/strategy/mutraveler/OneId.java
@@ -0,0 +1,35 @@
+package tom.library.strategy.mutraveler;
+import tom.library.strategy.mutraveler.reflective.AbstractVisitableVisitor;
+import jjtraveler.Visitable;
+import jjtraveler.reflective.VisitableVisitor;
+import jjtraveler.VisitFailure;
+
+/**
+ * <code>T(t1,..,ti,..,tN).accept(OneId(v)) = T(t1,...ti.accept(v),..,tN)</code>
+ * if <code>ti</code> is the first child that is modified.
+ * <p>
+ * Basic visitor combinator with one visitor argument, that applies
+ * this visitor to exactly one child. If no children are visited 
+ * successfully, then OneId(v) fails.
+ * <p>
+ * Note that side-effects of failing visits to children are not
+ * undone.
+ */
+
+public class OneId extends AbstractVisitableVisitor {
+  public OneId(VisitableVisitor v) {
+    init(v);
+  }
+
+  public Visitable visit(Visitable any) throws VisitFailure {
+    int childCount = any.getChildCount();
+    for (int i = 0; i < childCount; i++) {
+      Visitable newSubterm = getArgument(0).visit(any.getChildAt(i));
+      if (newSubterm != any.getChildAt(i)) {
+          return any.setChildAt(i,newSubterm);
+      } 
+    } 
+    return any;
+  }
+
+}
