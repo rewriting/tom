45fc9fa7e4197e3bb630f01abdedce8afa92ade4
Pierre-Etienne Moreau <Pierre-Etienne.Moreau@loria.fr>
2005-09-09 13:44:24 +0000

    *** empty log message ***

--- examples/aterm/ted/TedVisitor.java
+++ examples/aterm/ted/TedVisitor.java
@@ -5,17 +5,18 @@ import tom.library.strategy.mutraveler.*;
 
 public class TedVisitor extends aterm.Visitor {
 
-    Ted ted = new Ted();
-    ATerm tomatch;
+  Ted ted = new Ted();
+  ATerm tomatch;
 
-    public TedVisitor(ATerm tomatch) {
-	super();
-	this.tomatch = tomatch;
-    }
+  public TedVisitor(ATerm tomatch) {
+    super();
+    this.tomatch = tomatch;
+  }
 
-    public void visitATerm(ATerm arg) {
-	if (ted.match(tomatch, arg))
-	    System.out.println("matched !");
+  public void visitATerm(ATerm arg) {
+    if(ted.match(tomatch, arg)) {
+      System.out.println("matched !");
     }
+  }
 }
 
