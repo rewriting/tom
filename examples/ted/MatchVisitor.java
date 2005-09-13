98f9dfa404d9ab52e1c153c8841110d2fa842717
Antoine Reilles <Antoine.Reilles@loria.fr>
2005-09-13 15:39:26 +0000

    Move ted into a ted directory, and let ant build it - avoid package name clash with aterm - let the ted utility have his own ted package

---
+++ examples/ted/MatchVisitor.java
@@ -0,0 +1,24 @@
+package ted;
+
+import java.io.*;
+import aterm.*;
+import tom.library.strategy.mutraveler.*;
+
+
+public class MatchVisitor extends aterm.ATermFwdVoid {
+
+  Ted ted = new Ted();
+  ATerm tomatch;
+
+  public MatchVisitor(ATerm tomatch) {
+    super();
+    this.tomatch = tomatch;
+  }
+
+  public void voidVisitATerm(ATerm arg) {
+    if(ted.match(tomatch, arg)) {
+      System.out.println("matched !");
+    }
+  }
+}
+
