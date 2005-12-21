383cefed97c985884c9c21f21855b0eea211c87c
Antoine Reilles <Antoine.Reilles@loria.fr>
2005-12-21 14:41:43 +0000

    a first parser for minirho (to be renamed in xrho soon)

---
+++ examples/minirho/Main.java
@@ -0,0 +1,28 @@
+package minirho;
+
+import java.io.*;
+
+import aterm.pure.SingletonFactory;
+
+import minirho.rhoterm.*;
+import minirho.rhoterm.types.*;
+
+public class Main {
+  public static void main(String[] args) throws FileNotFoundException {
+    File file = new File(args[0]);
+
+    DataInputStream input = new DataInputStream(new FileInputStream(file));
+
+    // attach lexer to the input stream
+    RhoLexer lexer = new RhoLexer(input); // Create parser attached to lexer
+    RhoParser parser = new RhoParser(lexer);
+    // start up the parser by calling the rule
+    // at which you want to begin parsing. parser.rhoterm(); 
+    try {
+      RTerm term = parser.program();
+      System.out.println(term);
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
