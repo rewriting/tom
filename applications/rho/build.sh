fc35e2be15dfcf1e578e18440b6749be5f1166b4
Antoine Reilles <Antoine.Reilles@loria.fr>
2006-01-18 15:21:12 +0000

    Moved xrho from examples to application/rho add a buildfile. Those applications build files are ugly, should we provide in the application directory some helper pasks/paskdef/macros to help writing simple buildfiles for applications ?

---
+++ applications/rho/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# OS specific support.  $var _must_ be set to either true or false.
+cygwin=false;
+darwin=false;
+case "`uname`" in
+  CYGWIN*) cygwin=true ;;
+  Darwin*) darwin=true ;;
+esac
+
+# For Cygwin, ensure paths are in UNIX format before anything is touched
+if $cygwin ; then
+  [ -n "$TOM_HOME" ] &&
+    TOM_HOME=`cygpath --windows "$TOM_HOME"`
+fi
+
+LOCALCLASSPATH=""
+
+CLASSPATH=${LOCALCLASSPATH} ant -Dtoto=tutu $*
