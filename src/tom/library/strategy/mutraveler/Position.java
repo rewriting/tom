fe048a7bf84b70a283f25354be575187bfd9002d
Antoine Reilles <Antoine.Reilles@loria.fr>
2006-04-21 13:58:06 +0000

    formating

--- src/tom/library/strategy/mutraveler/Position.java
+++ src/tom/library/strategy/mutraveler/Position.java
@@ -14,32 +14,32 @@ public class Position {
   private static final int DEFAULT_LENGTH = 8;
   private int size;
   private int[] data;
-   
+
   public Position() {
     this(DEFAULT_LENGTH);
   }
 
   private Position(int length) {
     data = new int[length];
-		size = 0;
+    size = 0;
   }
-  
+
   private void ensureLength(int minLength) {
-		int current = data.length;
-		if (minLength > current) {
-			int[] newData = new int[Math.max(current * 2, minLength)];
-			System.arraycopy(data, 0, newData, 0, size);
-			data = newData;
-		}
-	}
+    int current = data.length;
+    if (minLength > current) {
+      int[] newData = new int[Math.max(current * 2, minLength)];
+      System.arraycopy(data, 0, newData, 0, size);
+      data = newData;
+    }
+  }
 
   protected Object clone() {
-		Position clone = new Position(data.length);
-		clone.size = size;
-		System.arraycopy(data, 0, clone.data, 0, size);
-		return clone;
+    Position clone = new Position(data.length);
+    clone.size = size;
+    System.arraycopy(data, 0, clone.data, 0, size);
+    return clone;
   }
- 
+
   /**
     * check if the position is empty
     *
@@ -48,14 +48,14 @@ public class Position {
   protected boolean isEmpty() {
     return size == 0;
   }
- 
+
   /**
    * Tests if two positions are equals
    */
   public boolean equals(Object o) {
     if (o instanceof Position) {
-			Position p = (Position)o;
-			return size==p.size && Arrays.equals(data,p.data);
+      Position p = (Position)o;
+      return size==p.size && Arrays.equals(data,p.data);
     } else {
       return false;
     }
@@ -65,15 +65,15 @@ public class Position {
    * Tests is prefix
    */
   public boolean isPrefix(Position p) {
-		if(p.size < size) {
-			return false;
-		}
-		for(int i=0 ; i<size ; i++) {
-			if(data[i] != p.data[i]) {
-				return false;
-			}
-		}
-		return true;
+    if(p.size < size) {
+      return false;
+    }
+    for(int i=0 ; i<size ; i++) {
+      if(data[i] != p.data[i]) {
+        return false;
+      }
+    }
+    return true;
   }
 
   /**
@@ -81,7 +81,7 @@ public class Position {
    * @return the current sub-position
    */
   protected int getSubPosition() {
-		return data[size];
+    return data[size];
   }
 
   /**
@@ -98,7 +98,7 @@ public class Position {
    * This may be a BAD idea
    */
   public void up() {
-		size--;
+    size--;
   }
 
   /**
@@ -108,11 +108,11 @@ public class Position {
    */
   public void down(int n) {
     if(n>0) {
-			if (size == data.length) {
-				ensureLength(size+1);
-			}
-			data[size++] = n;
-		}
+      if (size == data.length) {
+        ensureLength(size+1);
+      }
+      data[size++] = n;
+    }
   }
 
   /**
@@ -124,12 +124,12 @@ public class Position {
    */
   public VisitableVisitor getOmega(VisitableVisitor v) {
     VisitableVisitor res = v;
-		for(int i = size-1 ; i>=0 ; i--) {
+    for(int i = size-1 ; i>=0 ; i--) {
      res = new Omega(data[i],res);
-		}
+    }
     return res;
   }
-    
+
   /**
    * create s=omega(x->t)
    * such that s[subject] returns subject[t]|omega
@@ -148,7 +148,7 @@ public class Position {
    * @return the omega strategy that retrieves the corresponding subterm
    */
   public VisitableVisitor getSubterm() {
-   return new AbstractVisitableVisitor() { 
+   return new AbstractVisitableVisitor() {
      { initSubterm(); }
      public Visitable visit(Visitable subject) throws VisitFailure {
        final Visitable[] ref = new Visitable[1];
@@ -159,20 +159,20 @@ public class Position {
   }
 
   /**
-    * Returns a <code>String</code> object representing the position. 
+    * Returns a <code>String</code> object representing the position.
     * The string representation consists of a list of elementary positions
     *
     * @return a string representation of this position
     */
   public String toString() {
     StringBuffer r = new StringBuffer("[");
-		for(int i=0 ; i<size ; i++) {
-			r.append(data[i]);
-			if(i<size-1) {
-				r.append(", ");
-			}
-		}
-		r.append("]");
+    for(int i=0 ; i<size ; i++) {
+      r.append(data[i]);
+      if(i<size-1) {
+        r.append(", ");
+      }
+    }
+    r.append("]");
     return r.toString();
   }
 }
