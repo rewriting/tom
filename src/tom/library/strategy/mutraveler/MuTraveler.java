817fe7293e78d9305dc203cb9d7cda699b0db387
Pierre-Etienne Moreau <Pierre-Etienne.Moreau@loria.fr>
2006-04-27 07:23:07 +0000

    * added MuTraveler.getSubterm

--- src/tom/library/strategy/mutraveler/MuTraveler.java
+++ src/tom/library/strategy/mutraveler/MuTraveler.java
@@ -60,8 +60,8 @@ public class MuTraveler {
     return ((Position) positionMap.get(v)).getReplace(t);
 	}
 	
-	public static VisitableVisitor getSubterm(VisitableVisitor v, final Visitable t) {
-    return ((Position) positionMap.get(v)).getSubterm(t);
+	public static VisitableVisitor getSubterm(VisitableVisitor v) {
+    return ((Position) positionMap.get(v)).getSubterm();
 	}
 
 }
