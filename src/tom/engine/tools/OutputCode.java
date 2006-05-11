78cd106ae1706643c3a1b99d45b71365d5a81b20
Antoine Reilles <Antoine.Reilles@loria.fr>
2006-05-11 17:01:20 +0000

    details

--- src/tom/engine/tools/OutputCode.java
+++ src/tom/engine/tools/OutputCode.java
@@ -32,190 +32,190 @@ import java.io.Writer;
 import tom.platform.OptionManager;
 
 public class OutputCode {
-	protected Writer file;
-	private OptionManager optionManager;
+  protected Writer file;
+  private OptionManager optionManager;
 
-	private int lineCounter = 1;
-	private int singleLine = 0;
+  private int lineCounter = 1;
+  private int singleLine = 0;
 
-	private boolean pretty = false;
+  private boolean pretty = false;
 
-	public OutputCode(Writer file, OptionManager optionManager) {
-		this.file = file;
-		this.optionManager = optionManager; 
-		this.pretty = ((Boolean)optionManager.getOptionValue("pretty")).booleanValue();
-	}
+  public OutputCode(Writer file, OptionManager optionManager) {
+    this.file = file;
+    this.optionManager = optionManager;
+    this.pretty = ((Boolean)optionManager.getOptionValue("pretty")).booleanValue();
+  }
 
-	public OutputCode() {
-		this.file = new StringWriter();
-	}
+  public OutputCode() {
+    this.file = new StringWriter();
+  }
 
-	public void setSingleLine() {
-		this.singleLine++;
-	}
+  public void setSingleLine() {
+    this.singleLine++;
+  }
 
-	public void unsetSingleLine() {
-		this.singleLine--;
-	}
+  public void unsetSingleLine() {
+    this.singleLine--;
+  }
 
-	public Writer getFile() {
-		return file;
-	}
+  public Writer getFile() {
+    return file;
+  }
 
-	public void writeSpace() throws IOException {
-		file.write(' ');
-	}
-	public void writeOpenBrace() throws IOException {
-		file.write('(');
-	}
-	public void writeCloseBrace() throws IOException {
-		file.write(')');
-	}
-	public void writeComa() throws IOException {
-		file.write(',');
-	}
-	public void writeSemiColon() throws IOException {
-		file.write(';');
-	}
-	public void writeUnderscore() throws IOException {
-		file.write('_');
-	}
+  public void writeSpace() throws IOException {
+    file.write(' ');
+  }
+  public void writeOpenBrace() throws IOException {
+    file.write('(');
+  }
+  public void writeCloseBrace() throws IOException {
+    file.write(')');
+  }
+  public void writeComa() throws IOException {
+    file.write(',');
+  }
+  public void writeSemiColon() throws IOException {
+    file.write(';');
+  }
+  public void writeUnderscore() throws IOException {
+    file.write('_');
+  }
 
-	public void write(String s) throws IOException {
-		try {
-			file.write(s);
-		} catch (IOException e) {
-			System.out.println("write error");
-			e.printStackTrace();
-		}
-	}
+  public void write(String s) throws IOException {
+    try {
+      file.write(s);
+    } catch (IOException e) {
+      System.out.println("write error");
+      e.printStackTrace();
+    }
+  }
 
-	public void write(int n) throws IOException {
-		write(Integer.toString(n));
-	}
+  public void write(int n) throws IOException {
+    write(Integer.toString(n));
+  }
 
-	public void write(int deep,String s) throws IOException {
-		indent(deep);
-		write(s);
-	}
+  public void write(int deep,String s) throws IOException {
+    indent(deep);
+    write(s);
+  }
 
-	public void writeln() throws IOException {
-		if(pretty) {
-			file.write('\n');
-		}
-	}
+  public void writeln() throws IOException {
+    if(pretty) {
+      file.write('\n');
+    }
+  }
 
-	public void writeln(String s) throws IOException {
-		write(s);
-		writeln();
-	}
+  public void writeln(String s) throws IOException {
+    write(s);
+    writeln();
+  }
 
-	public void writeln(int deep,String s) throws IOException {
-		write(deep,s);
-		writeln();
-	}
+  public void writeln(int deep,String s) throws IOException {
+    write(deep,s);
+    writeln();
+  }
 
-	public void write(StringBuffer s) throws IOException {
-		write(s.toString());
-	}
+  public void write(StringBuffer s) throws IOException {
+    write(s.toString());
+  }
 
-	public void write(int deep,StringBuffer s) throws IOException {
-		write(deep,s.toString());
-	}
+  public void write(int deep,StringBuffer s) throws IOException {
+    write(deep,s.toString());
+  }
 
-	public void writeln(StringBuffer s) throws IOException {
-		writeln(s.toString());
-	}
+  public void writeln(StringBuffer s) throws IOException {
+    writeln(s.toString());
+  }
 
-	public void writeln(int deep,StringBuffer s) throws IOException {
-		writeln(deep,s.toString());
-	}
+  public void writeln(int deep,StringBuffer s) throws IOException {
+    writeln(deep,s.toString());
+  }
 
-	public void write(int deep, StringBuffer s, int line, int length) throws IOException {
-		write(deep, s.toString(), line, length);
-	}
+  public void write(int deep, StringBuffer s, int line, int length) throws IOException {
+    write(deep, s.toString(), line, length);
+  }
 
-	public void write(int deep, String s, int line, int length) throws IOException {
-		if(singleLine>0 && !((Boolean)optionManager.getOptionValue("cCode")).booleanValue()) {
-			s = s.replace('\n', ' ');
-			s = s.replace('\r', ' ');
-			s = s.replace('\t', ' ');
-			write(s);
-			return;
-		}
-		if (!pretty){
-			if(lineCounter > line) {
-				if(((Boolean)optionManager.getOptionValue("cCode")).booleanValue()) {
-					String s1 = "\n#line "+line+"\n";
-					// writeln(deep,s);
-					s = s1+s;
-				} else { // Java Stuff
-					length = 0;
-					s = s.replace('\n', ' ');
-					s = s.replace('\r', ' ');
-					s = s.replace('\t', ' ');
-					//System.out.println("remove:"+s);
-				}
-			} else if(lineCounter < line) {
-				while(lineCounter < line) {
-					write("\n");
-					lineCounter++;
-				}
-			}
-			lineCounter+= length;
-			write(s);
-		} else {
-		    String[] lines = s.split("\n");
-		    for (int i=0; i<lines.length-1; i++) {
-			String ln = lines[i];
-			ln = ln.replaceFirst("^\\s+",""); // removes spaces at the beginning of the line
-			writeln(deep, ln);
-		    }
-		    String ln = lines[lines.length-1];
-		    ln = ln.replaceFirst("^\\s+","");
-		    write(deep, ln);
-		}
-	}
+  public void write(int deep, String s, int line, int length) throws IOException {
+    if(singleLine>0 && !((Boolean)optionManager.getOptionValue("cCode")).booleanValue()) {
+      s = s.replace('\n', ' ');
+      s = s.replace('\r', ' ');
+      s = s.replace('\t', ' ');
+      write(s);
+      return;
+    }
+    if (!pretty){
+      if(lineCounter > line) {
+        if(((Boolean)optionManager.getOptionValue("cCode")).booleanValue()) {
+          String s1 = "\n#line "+line+"\n";
+          // writeln(deep,s);
+          s = s1+s;
+        } else { // Java Stuff
+          length = 0;
+          s = s.replace('\n', ' ');
+          s = s.replace('\r', ' ');
+          s = s.replace('\t', ' ');
+          //System.out.println("remove:"+s);
+        }
+      } else if(lineCounter < line) {
+        while(lineCounter < line) {
+          write("\n");
+          lineCounter++;
+        }
+      }
+      lineCounter+= length;
+      write(s);
+    } else {
+      String[] lines = s.split("\n");
+      for (int i=0; i<lines.length-1; i++) {
+        String ln = lines[i];
+        ln = ln.replaceFirst("^\\s+",""); // removes spaces at the beginning of the line
+        writeln(deep, ln);
+      }
+      String ln = lines[lines.length-1];
+      ln = ln.replaceFirst("^\\s+","");
+      write(deep, ln);
+    }
+  }
 
-	public void close() {
-		try {
-			file.flush();
-			file.close();
-		} catch (IOException e) {
-			System.out.println("close error");
-			e.printStackTrace();
-		}
-	}
+  public void close() {
+    try {
+      file.flush();
+      file.close();
+    } catch (IOException e) {
+      System.out.println("close error");
+      e.printStackTrace();
+    }
+  }
 
-	public String stringDump() {
-		try {
-			if(file instanceof StringWriter) {
-				file.flush();
-				return file.toString();
-			} else {
-				throw new InternalError("OutputCode does not contain any string");
-			}
-		} catch (IOException e) {
-			System.out.println("stringDump error");
-			e.printStackTrace();
-			return null;
-		}
-	}
+  public String stringDump() {
+    try {
+      if(file instanceof StringWriter) {
+        file.flush();
+        return file.toString();
+      } else {
+        throw new InternalError("OutputCode does not contain any string");
+      }
+    } catch (IOException e) {
+      System.out.println("stringDump error");
+      e.printStackTrace();
+      return null;
+    }
+  }
 
-	public void indent(int deep) {
-		try {
-			if(pretty) {
-				for(int i=0 ; i<deep ; i++) {
-					file.write(' ');
-					file.write(' ');
-				}
-			} else {
-				file.write(' ');
-			}
-		} catch (IOException e) {
-			System.out.println("write error");
-			e.printStackTrace();
-		}
-	}
+  public void indent(int deep) {
+    try {
+      if(pretty) {
+        for(int i=0 ; i<deep ; i++) {
+          file.write(' ');
+          file.write(' ');
+        }
+      } else {
+        file.write(' ');
+      }
+    } catch (IOException e) {
+      System.out.println("write error");
+      e.printStackTrace();
+    }
+  }
 
 }
