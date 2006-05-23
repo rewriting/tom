de389379ad9afaa0a284335782d537a017c4996c
Antoine Reilles <Antoine.Reilles@loria.fr>
2006-05-23 17:48:27 +0000

    Tell ant to use mome memory

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 LOCALCLASSPATH=`echo ./stable/lib/junit.jar | tr ' ' ':'` 
-CLASSPATH=${LOCALCLASSPATH} ant $*
+CLASSPATH=${LOCALCLASSPATH} ANT_OPTS="-XX:PermSize=128m -XX:MaxPermSize=128m" ant $*
