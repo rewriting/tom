9f53f60e9c9006ed88baa1835b0a35b264a43ff6
Antoine Reilles <Antoine.Reilles@loria.fr>
2006-06-04 09:30:37 +0000

    ./build.sh all do pass again

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-LOCALCLASSPATH=`echo ./stable/lib/junit.jar | tr ' ' ':'` 
+HERE=`pwd`
+LOCALCLASSPATH=`echo ${HERE}/stable/lib/junit.jar | tr ' ' ':'` 
 CLASSPATH=${LOCALCLASSPATH} ANT_OPTS="-XX:PermSize=128m -XX:MaxPermSize=128m" ant $*
