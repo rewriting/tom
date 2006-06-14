3dfcc0f6dde4e049fa400d8b5a1448c813945d46
Yoann Toussaint <Yoann.Toussaint@loria.fr>
2006-06-14 13:36:12 +0000

    bug fix: use java literals for non-ascii characters

--- applications/web-protheo/src/ToolsEncode.java
+++ applications/web-protheo/src/ToolsEncode.java
@@ -1,21 +1,9 @@
 public class ToolsEncode{
   public static String removeAccents(String s){
-    s = s.replaceAll("[éèêë]","e");
-    s = s.replaceAll("[ûù]","u");
-    s = s.replaceAll("[ïî]","i");
-    s = s.replaceAll("[àâ]","a");
-    s = s.replaceAll("[Ô]","o");
+    s = s.replaceAll("[\u00E8\u00E9\u00EB]","e");
 
-    s = s.replaceAll("[ü]","ue");
-    s = s.replaceAll("[ä]","ae");
-    s = s.replaceAll("[ö]","oe");
+    s = s.replaceAll("[\u00FC]","ue");
     
-    s = s.replaceAll("[ÈÉÊË]","E");
-    s = s.replaceAll("[ÛÙ]","U");
-    s = s.replaceAll("[ÏÎ]","I");
-    s = s.replaceAll("[ÀÂ]","A");
-    s = s.replaceAll("[Ô]","O");
-
     s = s.replaceAll(" ","");
     return s;
   }
