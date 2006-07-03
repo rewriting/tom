e024aa8ba928d179a45f7a44233ab405e9c9b775
Antoine Reilles <Antoine.Reilles@loria.fr>
2006-07-03 19:32:03 +0000

    generate a "good" file for metapost output

--- examples/gombench/crible.sh
+++ examples/gombench/crible.sh
@@ -5,20 +5,24 @@ listmax=500
 TEXFILE=crible.tex
 DATFILE=crible.dat
 
-echo "#size hand apigen gom" > ${DATFILE}
+echo "" > ${DATFILE}
+echo "" > ${DATFILE}.1.tmp
+echo "" > ${DATFILE}.2.tmp
+echo "" > ${DATFILE}.3.tmp
 echo "\hline" > ${TEXFILE}
 echo "Taille & Manuel & {\\apigen} & {\\tom} \\\\" >> ${TEXFILE}
 echo "\hline" >> ${TEXFILE}
 for i in `jot 10 $liststart $listmax`; do
 	echo -n "$i   & " >> ${TEXFILE}
-	echo -n "$i " >> ${DATFILE}
 	HANDTIME=`${JAVA} gombench.HandList $i | awk -F'&' '{printf $3}'`
 	echo -n "${HANDTIME} & " >> ${TEXFILE}
-	echo -n "${HANDTIME} " >> ${DATFILE}
+	echo "$i  ${HANDTIME} " >> ${DATFILE}.1.tmp
 	APITIME=`${JAVA} gombench.ApiList $i | awk -F'&' '{printf $3}'`
 	echo -n "${APITIME} & " >> ${TEXFILE}
-	echo -n "${APITIME} " >> ${DATFILE}
+	echo "$i  ${APITIME} " >> ${DATFILE}.2.tmp
 	GOMTIME=`${JAVA} gombench.GomList $i | awk -F'&' '{printf $3}'`
 	echo "${GOMTIME} \\\\" >> ${TEXFILE}
-	echo "${GOMTIME} " >> ${DATFILE}
+	echo "$i  ${GOMTIME} " >> ${DATFILE}.3.tmp
 done
+
+cat ${DATFILE}.1.tmp ${DATFILE}.2.tmp ${DATFILE}.3.tmp >> ${DATFILE}
