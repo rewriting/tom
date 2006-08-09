7188b7d1522e41c61f24c6862f9c6cda27bed6e6
Emilie Balland <Emilie.Balland@loria.fr>
2006-08-09 15:46:44 +0000

    add an interface for using term graphs (terms with references)

---
+++ src/tom/library/strategy/mutraveler/MuReference.java
@@ -0,0 +1,10 @@
+package tom.library.strategy.mutraveler;
+
+public interface MuReference extends jjtraveler.Visitable{
+
+    /**
+     * gives the array that represents the position of the reference
+     */
+    public int[] toArray();
+
+}
