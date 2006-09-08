ef6ac3c8ea3638bbaf492b766f8b987657872b44
Emilie Balland <Emilie.Balland@loria.fr>
2006-09-08 17:27:45 +0000

    clean up

--- examples/strategy/graphterm/GraphTermVisitor.java
+++
@@ -1,8 +0,0 @@
-package strategy.graphterm;
-
-public interface GraphTermVisitor {
-
-	public strategy.graphterm.types.GraphTerm visit_GraphTerm(strategy.graphterm.types.GraphTerm arg) throws jjtraveler.VisitFailure;
-	public strategy.graphterm.types.GraphTermList visit_GraphTermList(strategy.graphterm.types.GraphTermList arg) throws jjtraveler.VisitFailure;
-
-}
