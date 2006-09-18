a1c8e7baab49423ef76680770dd39fe2f9253f79
Pierre-Etienne Moreau <Pierre-Etienne.Moreau@loria.fr>
2006-09-18 10:13:22 +0000

    * added a new strategy Ref that traverse a reference * Ref(s)[ref(pos)] applies s to the position pos * Ref(s)[t] applies s to t if t is not a ref * there also exists RelativeRef, StrictRef (fails when t is not a ref), and StrictRelativeRef

---
+++ src/tom/library/strategy/mutraveler/RelativePosition.java
@@ -0,0 +1,49 @@
+package tom.library.strategy.mutraveler;
+
+/**
+ * Object that represents a relative position in a term
+ * the first element in the list represents the number of backtracking
+ * steps and the others classicals steps in the term
+ */
+
+public class RelativePosition extends Position {
+
+  public RelativePosition(int[] data) {
+    super(data);
+  }
+
+  public static RelativePosition make(Position source,Position target) {
+    int[] sourceData = source.toArray();
+    int[] targetData = target.toArray();
+    int min_length =Math.min(sourceData.length,targetData.length); 
+    int commonPrefixLength=0;
+    while(commonPrefixLength<min_length && sourceData[commonPrefixLength]==targetData[commonPrefixLength]){
+      commonPrefixLength++;
+    }
+    int[] relativeData = new int[target.depth()-commonPrefixLength+1];
+    relativeData[0]=source.depth()-commonPrefixLength;
+    for(int j=1;j<relativeData.length;j++){
+      relativeData[j] = targetData[commonPrefixLength+j-1];
+    }
+    RelativePosition res = new RelativePosition(relativeData);
+    return res;
+
+  }
+
+  public Position getAbsolutePosition(Position currentPosition) {
+    int[] currentData = currentPosition.toArray();
+    int prefix = currentPosition.depth()-data[0];
+    int absoluteDataLength = prefix+depth()-1;
+    int[] absoluteData = new int[absoluteDataLength];
+    for(int i=0 ; i<prefix ; i++) {
+      absoluteData[i]=currentData[i];
+    }
+    for(int i=prefix ; i<absoluteDataLength ; i++){
+      absoluteData[i]=data[i-prefix+1];
+    }
+    Position absolutePos = new Position(absoluteData);
+    System.out.println("absoluteposition = " + absolutePos);
+    return absolutePos;
+  }
+
+}
