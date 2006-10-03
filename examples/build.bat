8fd12b8b3ea27224eba1c979e39f4477d0f9df4d
Radu Kopetz <Radu.Kopetz@loria.fr>
2006-10-03 11:42:28 +0000

    the build.bat for the examples

---
+++ examples/build.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set LOCALCLASSPATH=./lib/junit.jar
+set CLASSPATH=%LOCALCLASSPATH%
+
+rem Get command line arguments and save them in 
+rem the CMD_LINE_ARGS environment variable
+set CMD_LINE_ARGS=
+:setArgs
+if ""%1""=="""" goto doneSetArgs
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+shift
+goto setArgs
+:doneSetArgs
+
+echo %CMD_LINE_ARGS%
+
+ant %CMD_LINE_ARGS%
