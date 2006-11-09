171bddc63670feb26b447a214fa953bfcf1fee23
Antoine Reilles <Antoine.Reilles@loria.fr>
2006-11-09 19:26:57 +0000

    spaces

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 set LOCALCLASSPATH=./stable/lib/junit.jar
 set CLASSPATH=%LOCALCLASSPATH%
 
-rem Get command line arguments and save them in 
+rem Get command line arguments and save them in
 rem the CMD_LINE_ARGS environment variable
 set CMD_LINE_ARGS=
 :setArgs
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 HERE=`pwd`
-LOCALCLASSPATH=`echo ${HERE}/stable/lib/junit.jar | tr ' ' ':'` 
+LOCALCLASSPATH=`echo ${HERE}/stable/lib/junit.jar | tr ' ' ':'`
 CLASSPATH=${LOCALCLASSPATH} ANT_OPTS="-XX:PermSize=128m -XX:MaxPermSize=128m" ant $*
