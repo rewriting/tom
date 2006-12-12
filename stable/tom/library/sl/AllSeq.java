110c63c0fab59e690270c400c354f5c538ea9174
Antoine Reilles <Antoine.Reilles@loria.fr>
2006-12-12 13:44:28 +0000

    missing files

---
+++ stable/tom/library/sl/AllSeq.java
@@ -0,0 +1,71 @@
+/*
+ *
+ * Copyright (c) 2000-2006, Pierre-Etienne Moreau
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 	- Redistributions of source code must retain the above copyright
+ * 	notice, this list of conditions and the following disclaimer.
+ * 	- Redistributions in binary form must reproduce the above copyright
+ * 	notice, this list of conditions and the following disclaimer in the
+ * 	documentation and/or other materials provided with the distribution.
+ * 	- Neither the name of the INRIA nor the names of its
+ * 	contributors may be used to endorse or promote products derived from
+ * 	this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **/
+package tom.library.sl;
+
+/**
+ * <code>AllSeq(v).visit(T(t1,...,tN) = T(v.visit(t1), ..., v.visit(t1))</code>
+ * <p>
+ * Basic visitor combinator with one visitor argument, that applies
+ * this visitor to all children.
+ */
+
+public class AllSeq extends AbstractStrategy {
+  public final static int ARG = 0;
+
+  public AllSeq(Strategy v) {
+    initSubterm(v);
+  }
+
+  public final jjtraveler.Visitable visit(jjtraveler.Visitable any) throws jjtraveler.VisitFailure {
+    int childCount = any.getChildCount();
+    jjtraveler.Visitable result = any;
+    for (int i = 0; i < childCount; i++) {
+      jjtraveler.Visitable newChild = visitors[ARG].visit(result.getChildAt(i));
+      result = result.setChildAt(i, newChild);
+    }
+    return result;
+  }
+
+  public void visit() {
+    int childCount = getSubject().getChildCount();
+    for (int i = 0; i < childCount; i++) {
+      environment.down(i+1);
+      (visitors[ARG]).visit();
+      if (getStatus() != Environment.SUCCESS) {
+        environment.up();
+        return;
+      } else {
+        /* no need to restore subject */
+        environment.up();
+      }
+    }
+  }
+}
---
+++ stable/tom/library/sl/Omega.java
@@ -0,0 +1,82 @@
+/*
+ *
+ * Copyright (c) 2000-2006, Pierre-Etienne Moreau
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 	- Redistributions of source code must retain the above copyright
+ * 	notice, this list of conditions and the following disclaimer.
+ * 	- Redistributions in binary form must reproduce the above copyright
+ * 	notice, this list of conditions and the following disclaimer in the
+ * 	documentation and/or other materials provided with the distribution.
+ * 	- Neither the name of the INRIA nor the names of its
+ * 	contributors may be used to endorse or promote products derived from
+ * 	this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **/
+package tom.library.sl;
+
+/**
+ * <code>Omega(i,v)</code>
+ * <p>
+ * Basic visitor combinator which applies v the i-th subterm
+ * 0-th subterm is the term itself
+ * 1-th subterm corresponds to the first subterm
+ * ...
+ * arity-th subterm corresponds to the last subterm
+ * <p>
+*/
+
+public class Omega extends AbstractStrategy {
+  public final static int ARG = 0;
+  protected int indexPosition;
+
+  public Omega(int indexPosition, Strategy v) {
+    initSubterm(v);
+    this.indexPosition = indexPosition;
+  }
+
+  public int getPos() {
+    return indexPosition;
+  }
+
+  public jjtraveler.Visitable visit(jjtraveler.Visitable any) throws jjtraveler.VisitFailure {
+    if(indexPosition==0) {
+      return any;
+    } else if(indexPosition>0 && indexPosition<=any.getChildCount()) {
+      int childNumber = indexPosition-1;
+      jjtraveler.Visitable newChild = visitors[ARG].visit(any.getChildAt(childNumber));
+      return any.setChildAt(childNumber,newChild);
+    } else {
+      throw new jjtraveler.VisitFailure();
+    }
+  }
+
+  public void visit() {
+    if(indexPosition==0) {
+      return;
+    } else if(indexPosition>0 && indexPosition<=getSubject().getChildCount()) {
+      int childNumber = indexPosition-1;
+      environment.down(indexPosition);
+      (visitors[ARG]).visit();
+      environment.up();
+      return ;
+    } else {
+      setStatus(Environment.FAILURE);
+    }
+  }
+}
