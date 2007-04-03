bba62df01b69bda1622b8a4804b49e4832b28526
Radu Kopetz <Radu.Kopetz@loria.fr>
2007-04-03 07:17:09 +0000

    merge trunk changes: from 5579 to 5758

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 HERE=`pwd`
-LOCALCLASSPATH=`echo ${HERE}/stable/lib/junit.jar | tr ' ' ':'`
-CLASSPATH=${LOCALCLASSPATH} ANT_OPTS="-XX:PermSize=128m -XX:MaxPermSize=128m" ant $*
+LOCALCLASSPATH=`echo $/home/radu/workspace/constraintCompiler/stable/lib/junit.jar | tr ' ' ':'`
+#CLASSPATH=${LOCALCLASSPATH} ANT_OPTS="-Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=128m" ant with.clover $*
+CLASSPATH=${LOCALCLASSPATH} ANT_OPTS="-Xmx512m -XX:PermSize=128m -XX:MaxPermSize=128m" ant $*
