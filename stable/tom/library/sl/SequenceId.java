26c24f162793d5d296778336ee5bba25eb90a6af
Antoine Reilles <Antoine.Reilles@loria.fr>
2007-04-10 11:09:40 +0000

    added files

---
+++ stable/tom/library/sl/SequenceId.java
@@ -0,0 +1,69 @@
+/*
+ *
+ * Copyright (c) 2000-2007, Pierre-Etienne Moreau
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 	- Redistributions of source code must retain the above copyright
+ * 	notice, this list of conditions and the following disclaimer.
+ * 	- Redistributions in binary form must reproduce the above copyright
+ * 	notice, this list of conditions and the following disclaimer in the
+ * 	documentation and/or other materials provided with the distribution.
+ * 	- Neither the name of the INRIA nor the names of its
+ * 	contributors may be used to endorse or promote products derived from
+ * 	this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **/
+package tom.library.sl;
+
+/**
+ * <code>x.fire(SequenceId (v1,v2)) = x.fire(v1) ; x.fire(v2) if x.fire(v1)!=x</code>
+ * <p>
+ * Basic visitor combinator with two visitor arguments, that applies
+ * these visitors one after the other (sequential composition), if the first
+ * one is not the identity.
+ */
+
+public class SequenceId   extends AbstractStrategy {
+  public final static int FIRST = 0;
+  public final static int THEN = 1;
+  public SequenceId (Strategy first, Strategy then) {
+    initSubterm(first,then);
+  }
+
+  public jjtraveler.Visitable visit(jjtraveler.Visitable visitable) throws jjtraveler.VisitFailure {
+    jjtraveler.Visitable v = visitors[FIRST].visit(visitable);
+    if(v != visitable) {
+      return visitors[THEN].visit(v);
+    } else {
+      return v;
+    }
+  }
+
+  public void visit() {
+    Visitable subject = getEnvironment().getSubject();
+    visitors[FIRST].visit();
+    if (getStatus() == Environment.SUCCESS && ! getEnvironment().getSubject().equals(subject)) {
+        visitors[THEN].visit();
+    } else {
+      /* restore the subject */
+      /* we are juste intersted in the status */
+      getEnvironment().setSubject(subject);
+    }
+  }
+
+}
