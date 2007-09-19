5e6bbdf60eba676d574b8e7436d9be9d7eaf05a0
Radu Kopetz <Radu.Kopetz@loria.fr>
2007-09-19 16:00:09 +0000

    custom class loader

---
+++ applications/mappingGenerator/src/MGClassLoader.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+
+public class MGClassLoader extends URLClassLoader {
+
+  public MGClassLoader(URL[] urls) {
+    super(urls);
+  }
+
+  public Class<?> getClassObj(File file) throws ClassNotFoundException, FileNotFoundException, IOException{
+    // TODO - check the cast ... 
+    byte[] classBytes = new byte[(int)file.length()];
+    new FileInputStream(file).read(classBytes);
+    return defineClass(null,classBytes,0,classBytes.length);
+  }
+}
