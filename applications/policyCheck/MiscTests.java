2cd51c5e4e68dae33607467703b27766f8f0e232
Horatiu Cirstea <Horatiu.Cirstea@loria.fr>
2007-11-09 14:09:06 +0000

    useless Permutations

--- applications/policyCheck/MiscTests.java
+++ applications/policyCheck/MiscTests.java
@@ -5,12 +5,13 @@ public class MiscTests {
 	public static void main(String[] args) {
 
     //		Combinations 
+		System.out.println("COMBINATIONS ---------------------");
 		Combinations toto=new Combinations(3,2);
-    // 		toto.run();
 		System.out.println(toto.getCombination());
 
     // PermutationGenerator
-    PermutationGenerator pg = new  PermutationGenerator(4);
+		System.out.println("PermutationGenerator ---------------------");
+    PermutationGenerator pg = new  PermutationGenerator(10);
     int nbt = Integer.parseInt(pg.getTotal().toString());
 		System.out.println(nbt+" permutations");
     for(int i=0; i< nbt; i++){
@@ -19,6 +20,14 @@ public class MiscTests {
         System.out.print(perm[j]+" ");
       }
       System.out.println("");
+    }
+
+    //		Permutations 
+		System.out.println("PERMUTATIONS ---------------------");
+    Permutations titi=new Permutations(10);
+		System.out.println(titi.getPermutation().size() + " : " + titi.getPermutation());
+    while(titi.hasMore()){
+      System.out.println(titi.getNextPermutation());
     }
 
 	}
