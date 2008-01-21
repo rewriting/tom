181fb6e45beff3566e151abd5c3381c6cdd65ece
Yassine Guebbas <Yassine.Guebbas@mines.inpl-nancy.fr>
2008-01-21 21:03:02 +0000

    modification on class name

--- applications/policyCheck/src/policy/Policy.java
+++
@@ -1,8 +0,0 @@
-package policy;
-import accesscontrol.*;
-import accesscontrol.types.*;
-
-public interface Policy {
-  public Response transition(RequestUponState rus);
-  public boolean valid(State setOfAccesses);
-}
