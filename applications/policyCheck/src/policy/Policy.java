53b61b4819c5412899031637ff35af8a4f95d281
Horatiu Cirstea <Horatiu.Cirstea@loria.fr>
2008-02-13 16:39:59 +0000

    add policy type

--- applications/policyCheck/src/policy/Policy.java
+++ applications/policyCheck/src/policy/Policy.java
@@ -4,18 +4,13 @@ import accesscontrol.*;
 import accesscontrol.types.*;
 
 /**
- * Generic policy defined by the lattice of security levels
- * and by a current state containing the current (read/write) accesses
+ * Generic policy 
  *
  * @author 
  * @version
  * 
  */
-public abstract class Policy {
-
-  private SecurityLevelsLattice slL;
-
-  private State currentState;
+public interface Policy {
 
 	/**
 	 * The implementation of the policy: returns a decision for a given request
@@ -23,13 +18,13 @@ public abstract class Policy {
 	 * @param r the access request 
 	 * @return the decision for the demanded request
 	 */
-	public abstract Decision transition(Request r);
+	public  Decision transition(Request r);
 
 	/**
 	 * The predicate that should be verified by  the policy
 	 * 
 	 * @return true if the current state respects the predicate, false otherwise
 	 */
-	public abstract boolean valid();
+	public  boolean valid();
 	
 }
