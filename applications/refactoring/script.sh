f6406f1153a89524f14507133fb7727c7a2a7fce
Emilie Balland <Emilie.Balland@loria.fr>
2008-04-25 00:36:02 +0000

    * improve the script

--- applications/refactoring/script.sh
+++ applications/refactoring/script.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
-
-mkdir -p /tmp/testgen
-mkdir -p /tmp/output/failure
-cd /tmp/testgen
-i = 0 ;
-j = 0 ;
-for   [ $i -lt 10 ] ; do
-  java testgen/Generator > log
+testgendir=`mktemp -d /tmp/testgen.XXXXX`
+outputdir=`mktemp -d /tmp/testoutput.XXXXX`
+failuredir=$outputdir/failure
+currentdir=$PWD
+mkdir $failuredir
+cd $testgendir
+i=0 ;
+j=0 ;
+while [ $i -lt $1 ] ; do
+  java testgen/Generator > log 2>&1
   if [ $? = 0 ] ; then
     find . -name '*.java' | xargs javac >> log 2>&1
     if [ $? != 0 ] ; then 
       echo "Failure(s) during java compilation (see log file)"
-      mv log /tmp/output/failure/log$i
-      rm -rf *
+      mv log $failuredir/log${i}javac
+      rm -rf $testgendir/*
     else 
-      mkdir /tmp/output/test$j
-      mv * /tmp/output/test$j
-      j = $((j+1))
+      mkdir $outputdir/test$j
+      mv $testgendir/* $outputdir/test$j
+      j=$((j+1))
     fi
   else 
     echo "Failure(s) during hierarchy generation (see log file)"
-    mv log /tmp/output/failure/log$i
-    rm -rf *
+    mv log $failuredir/log${i}generation
+    rm -rf $testgendir/*
   fi
-  i = $((i+1))
+  i=$((i+1))
 done
+echo "Success rate : $j/$i"
+mv $outputdir $currentdir/
