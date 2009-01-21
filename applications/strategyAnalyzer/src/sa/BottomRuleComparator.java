8921dced78c217fceac20f0907ceae43db0c1c2a
Horatiu Cirstea <Horatiu.Cirstea@loria.fr>
2009-01-21 09:56:02 +0000

    Comp

---
+++ applications/strategyAnalyzer/src/sa/BottomRuleComparator.java
@@ -0,0 +1,14 @@
+package sa;
+
+import java.util.*;
+import sa.rule.types.*;
+
+public class BottomRuleComparator implements Comparator<Rule>{
+  public int compare(Rule r1, Rule r2){
+    if(Pretty.toString(r1.getrhs(),true).compareTo("Bottom")==0 ){
+      return -10;
+    } else {
+      return r1.getlhs().toString().compareTo(r2.getlhs().toString());
+    }
+  }
+}
