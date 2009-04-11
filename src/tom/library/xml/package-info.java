22ab97911d4e2db1862826933e6e14a4003a380b
Antoine Reilles <Antoine.Reilles@loria.fr>
2009-04-11 10:03:08 +0000

    add package description

---
+++ src/tom/library/xml/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides the Tom XML library.
+ * <p>
+ * This package provides tools to convert back and forth between DOM XML
+ * representation and TNode (a Gom structure for XML).
+ *
+ * @see tom.library.adt.tnode.types.TNode
+ */
+package tom.library.xml;
