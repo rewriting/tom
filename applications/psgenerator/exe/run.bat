dc739f77190b72e26b31d94212850b7617fae90f
Pierre-Etienne Moreau <Pierre-Etienne.Moreau@loria.fr>
2009-06-11 07:27:24 +0000

    * moved some directories into poc * add a new empty directory for prototyping the new mapping based on java interfaces

--- applications/psgenerator/exe/run.bat
+++
@@ -1,4 +0,0 @@
-@echo try -Djava.util.logging.config.file=logging.properties for logging
-@echo try -ea for tracing... will create a big psgen.trace file
-@java -cp %CLASS_PATH%;..\lib\antlr.jar;..\lib\apigen.jar;..\lib\aterm.jar;..\lib\concurrent.jar;..\lib\jtom.jar;..\lib\mutraveler.jar;..\lib\PlatformOption.jar;..\lib\plugin-platform.jar;..\lib\Set.jar;..\lib\shared-objects.jar;..\lib\TNode.jar;..\lib\tom-library.jar;..\lib\TomSignature.jar;..\lib\vas-to-adt.jar;..\lib\commandline.jar;..\lib\psgen.jar fr.loria.protheo.psgenerator.Generator -o:clips ../examples/houses.pss
-@pause
