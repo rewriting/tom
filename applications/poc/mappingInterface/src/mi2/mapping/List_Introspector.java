78087e261505244967c8a871ec5a455977b82963
Nick Vintila <nvintila@yahoo.com>
2009-06-24 01:30:26 +0000

    Small cleanup. List_Introspector methods really need to be implemented?

--- applications/poc/mappingInterface/src/mi2/mapping/List_Introspector.java
+++ applications/poc/mappingInterface/src/mi2/mapping/List_Introspector.java
@@ -8,35 +8,40 @@ import java.util.List;
  */
 public abstract class List_Introspector<T> extends IntrospectorMapping<List<T>> implements ListMapping_I<T> {
 
-    public T setChildren(Object o, Object[] children) {
-      //TODO
-      return null;
+    public List<T> setChildren(Object o, Object[] children) {
+        //TODO
+        assert false : "Unexpected call!";
+        return null;
     }
 
     public Object[] getChildren(Object o) {
-      //TODO
-      return null;
+        //TODO
+        assert false : "Unexpected call!";
+        return null;
     }
 
     public /*<T> T*/ Object setChildAt(/*T*/Object o, int i, Object child) {
-      //TODO
-      return null;
+        //TODO
+        assert false : "Unexpected call!";
+        return null;
     }
 
     public Object getChildAt(Object o, int i) {
-      //TODO
-      return null;
+        //TODO
+        assert false : "Unexpected call!";
+        return null;
     }
 
     public int getChildCount(Object o) {
-      if (isEmpty((List<T>)o)) {
-        return 0;
-      } else {
-        return ((List<T>)o).size();//2; //todo why 2? pass in constructor??
-      }
+
+//        if (isEmpty((List<T>) o)) {
+//            return 0;
+//        } else {
+        return ((List<T>) o).size();//2; //todo why 2? pass in constructor??
+//        }
     }
 
     public Class forType() {
-      return List.class;
+        return List.class;
     }
 }
