57bdd8aae47df14b10d6661adc7f0ef834678bbb
Pierre-Etienne Moreau <Pierre-Etienne.Moreau@loria.fr>
2009-07-01 15:00:40 +0000

    * mi3 improved again

--- applications/poc/mappingInterface/src/mi3/gom/Module.java
+++ applications/poc/mappingInterface/src/mi3/gom/Module.java
@@ -9,93 +9,14 @@ import tom.library.sl.Introspector;
 import mi3.mapping.*;
 
 public class Module {
+  /*
+   * this file is hand-written (or generated for Gom data-structures)
+   */
 
   public static final Module instance = new Module();
 
 
-  // split SL introspectors
-  public interface Introspector1 {
-    public <T> T setChildren(T o, Object[] children);
-    public Object[] getChildren(Object o);
-  }
-
-  public interface Introspector2 {
-    public <T> T setChildAt(T o, int i, Object child);
-    public Object getChildAt(Object o, int i);
-    public int getChildCount(Object o);
-  }
-
-  public class IntrospectorFactory {
-
-    public Introspector makeFromIntrospector1(final Introspector1 introspector) {
-      return new Introspector() {
-
-        public <T> T setChildAt(T o, int i, Object child) {
-          Object[] children = getChildren(o);
-          children[i] = child;
-          return setChildren(o, children);
-        }
-
-        public Object getChildAt(Object o, int i) {
-          return getChildren(o)[i];
-        }
-
-        public int getChildCount(Object o) {
-          return getChildren(o).length;
-        }
-
-        public <T> T setChildren(T o, Object[] children) {
-          return introspector.setChildren(o, children);
-        }
-
-        public Object[] getChildren(Object o) {
-          return introspector.getChildren(o);
-        }
-      };
-    }
-
-    public Introspector makeFromInstrospector2(final Introspector2 introspector) {
-      return new Introspector() {
-
-        public <T> T setChildAt(T o, int i, Object child) {
-          return introspector.setChildAt(o, i, child);
-        }
-
-        public Object getChildAt(Object o, int i) {
-          return introspector.getChildAt(o, i);
-        }
-
-        public int getChildCount(Object o) {
-          return introspector.getChildCount(o);
-        }
-
-        //FIX in case of normalization rules
-        public <T> T setChildren(T o, Object[] children) {
-          for (int i = 0; i<children.length ; i++) {
-            o = setChildAt(o, i, children[i]);
-          }
-          return o;
-        }
-
-        public Object[] getChildren(Object o) {
-          Object[] children = new Object[getChildCount(o)];
-          for (int i = 0; i< getChildCount(o); i++) {
-            children[i] = getChildAt(o, i);
-          }
-          return children;
-        }
-      };
-    }
-
-  }
-
   //implementation of the interfaces defined by users
-
-  public static a_Mapping a_Mapping = new a_Mapping();
-  public static b_Mapping b_Mapping = new b_Mapping();
-  public static f_Mapping f_Mapping = new f_Mapping();
-  public static g_Mapping g_Mapping = new g_Mapping();
-
   public static class a_Mapping extends Mapping0<T1> {
     public Class getImplementation() {
       return a.class;
@@ -173,5 +94,82 @@ public class Module {
 
   }
 
+  /*
+   * Not used at present
+   */
+  // split SL introspectors
+  public interface Introspector1 {
+    public <T> T setChildren(T o, Object[] children);
+    public Object[] getChildren(Object o);
+  }
 
+  public interface Introspector2 {
+    public <T> T setChildAt(T o, int i, Object child);
+    public Object getChildAt(Object o, int i);
+    public int getChildCount(Object o);
+  }
+
+  public class IntrospectorFactory {
+
+    public Introspector makeFromIntrospector1(final Introspector1 introspector) {
+      return new Introspector() {
+
+        public <T> T setChildAt(T o, int i, Object child) {
+          Object[] children = getChildren(o);
+          children[i] = child;
+          return setChildren(o, children);
+        }
+
+        public Object getChildAt(Object o, int i) {
+          return getChildren(o)[i];
+        }
+
+        public int getChildCount(Object o) {
+          return getChildren(o).length;
+        }
+
+        public <T> T setChildren(T o, Object[] children) {
+          return introspector.setChildren(o, children);
+        }
+
+        public Object[] getChildren(Object o) {
+          return introspector.getChildren(o);
+        }
+      };
+    }
+
+    public Introspector makeFromInstrospector2(final Introspector2 introspector) {
+      return new Introspector() {
+
+        public <T> T setChildAt(T o, int i, Object child) {
+          return introspector.setChildAt(o, i, child);
+        }
+
+        public Object getChildAt(Object o, int i) {
+          return introspector.getChildAt(o, i);
+        }
+
+        public int getChildCount(Object o) {
+          return introspector.getChildCount(o);
+        }
+
+        //FIX in case of normalization rules
+        public <T> T setChildren(T o, Object[] children) {
+          for (int i = 0; i<children.length ; i++) {
+            o = setChildAt(o, i, children[i]);
+          }
+          return o;
+        }
+
+        public Object[] getChildren(Object o) {
+          Object[] children = new Object[getChildCount(o)];
+          for (int i = 0; i< getChildCount(o); i++) {
+            children[i] = getChildAt(o, i);
+          }
+          return children;
+        }
+      };
+    }
+
+  }
 }
