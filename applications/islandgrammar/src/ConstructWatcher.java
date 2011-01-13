f9a1c2da51f86f61268cd4621ca3da4c43f0b1b8
Ubuntu <ubuntu@ubuntu-desktop.(none)>
2011-01-13 12:26:40 -0700

    "La méthode take est void maintenant"

--- applications/islandgrammar/src/ConstructWatcher.java
+++ applications/islandgrammar/src/ConstructWatcher.java
@@ -1,19 +1,20 @@
+
 public class ConstructWatcher {
 
-  private int state;
-  private String keyword;
+    private int state;
+    private String keyword;
 
-  public ConstructWatcher(String keyword) {
-    state = 0;
-    this.keyword = keyword;
-  }
-
-  public boolean take(char c) {
-    if(keyword.charAt(state) == c) {
-      state++;
-      return true;
+    public ConstructWatcher(String keyword) {
+        this.state = 0;
+        this.keyword = keyword;
     }
-    return false;
-  }
 
+    public void take(char c) {
+        if (keyword.charAt(state) == c) {
+            state++;
+            return ;
+        }
+        state = 0;
+        return ;
+    }
 }
