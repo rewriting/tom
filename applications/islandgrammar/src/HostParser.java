68b10e4357014016170b505eac841f8a50268036
Ubuntu <ubuntu@ubuntu-desktop.(none)>
2011-01-13 12:24:54 -0700

    "Essai pour detecter les opérateurs tom génériquement"

--- applications/islandgrammar/src/HostParser.java
+++ applications/islandgrammar/src/HostParser.java
@@ -1,29 +1,42 @@
+
 import org.antlr.runtime.*;
-import java.lang.StringBuffer;
 
 public class HostParser {
 
-  private CharStream input;
-  private StringBuffer hostContent;
+    private CharStream input;
+    private StringBuffer hostContent;
 
-  public HostParser(CharStream input) {
-    this.input = input;
-    hostContent = new StringBuffer();
-  }
-
-  public void parse() {
-    
-    while(true) {
-      char truc = (char) input.LA(1);
-      if(truc == (char) -1) {
-        break;
-      }
-      hostContent.append(truc);
-      input.consume();
+    public HostParser(CharStream input) {
+        this.input = input;
+        hostContent = new StringBuffer();
     }
-  }
 
-  public String getCode() {
-    return hostContent.toString();
-  }
+    public void parse() {
+        ConstructWatcher match = new ConstructWatcher("%match");
+        ConstructWatcher op = new ConstructWatcher("%op");
+        ConstructWatcher[] listconstructors = ConstructWatcher[2];
+        listconstructors[0] = match;
+        listconstructors[1] = op;
+        while (true) {
+            char truc = (char) input.LA(1);
+            match.take(truc);
+            op.take(truc);
+            if (truc == (char) -1) {
+                break;
+            }
+            for (int i = 0; i < listconstructors.length(); i++) {
+                {
+                    if (listconstructors[i].state == listconstructors[i].keyword.length()) {
+                        //
+                    }
+                }
+                hostContent.append(truc);
+                input.consume();
+            }
+        }
+    }
+
+    public String getCode() {
+        return hostContent.toString();
+    }
 }
