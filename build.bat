c1e4c63c8f3f1f6b7d21c6e35be2d18793aaedd2
Francois <francois.prugniel@loria.fr>
2011-02-14 11:09:27 +0100

    * Fix Windows compilation

--- build.bat
+++ build.bat
@@ -1,12 +1,15 @@
 @echo off
 setlocal
 
-set LOCALCLASSPATH=./stable/lib/tools/junit.jar
-set CLASSPATH=%LOCALCLASSPATH%
+set LOCALCLASSPATH=./stable/lib/compiletime/junit.jar;./stable/lib/compiletime/ant-launcher.jar;./stable/lib/compiletime/ant-nodeps.jar;./stable/lib/compiletime/ant.jar;./stable/lib/compiletime/ant-antlr.jar;./stable/lib/compiletime/antlr3.jar;./stable/lib/compiletime/ant-junit.jar
+set CLASSPATH=.
+
+set ANT_OPTS=-Xmx512m -XX:PermSize=128m -XX:MaxPermSize=128m
+set ant_exec_command=java %ANT_OPTS% -classpath %LOCALCLASSPATH% -Dant.library.dir=./stable/lib/compiletime org.apache.tools.ant.launch.Launcher %ANT_ARGS% -cp %CLASSPATH%
 
 rem Get command line arguments and save them in
 rem the CMD_LINE_ARGS environment variable
-set CMD_LINE_ARGS=
+set CMD_LINE_ARGS=%ant_exec_command%
 :setArgs
 if ""%1""=="""" goto doneSetArgs
 set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
@@ -14,6 +17,6 @@ shift
 goto setArgs
 :doneSetArgs
 
-echo %CMD_LINE_ARGS%
+rem echo %CMD_LINE_ARGS%
 
-ant %CMD_LINE_ARGS%
+%CMD_LINE_ARGS%
