71d6f5f9a76222ccfaedc1b6bfabe043cbd1591c
Remi Bouchez <remi.bouchez@mines-nancy.org>
2011-02-16 19:08:15 +0100

    ppmain improved

--- src/tom/engine/prettyprinter/ppeditor/src/Main.java
+++ src/tom/engine/prettyprinter/ppeditor/src/Main.java
@@ -4,13 +4,23 @@ public class Main {
 
   public static void main(String[] args){
 
-  String texte1="Première ligne";
-  String texte2="Troisième ligne\nEt quatrième si tout va bien";
+  /*Step one: writing were there was nothing before (insertion mode)*/
+  String text1="Première ligne";
+  String text2="Troisième ligne\n\nEt cinquième si tout va bien";
   PPCursor aCursor = new PPCursor(0,0);
 
-  aCursor.write(texte1);
+  aCursor.write(text1);
   aCursor.setPosition(new PPTextPosition(2,0));
-  aCursor.write(texte2);
+  aCursor.write(text2);
+
+  /*Step two: writing inside of a line (insertion mode)*/
+  String text3="Septième ligne";
+  String text4="// Insertion entre \"Sept\" et \"ième ligne\" //";
+
+  aCursor.setPosition(new PPTextPosition(6,0));
+  aCursor.write(text3);
+  aCursor.setPosition(new PPTextPosition(6,4));
+  aCursor.write(text4);
 
   aCursor.dump("./Test.txt");
   }
