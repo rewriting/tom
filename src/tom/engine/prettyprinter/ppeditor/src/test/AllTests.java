da0aa31f1a1b4f4780e69d1daee24be7ffdc6992
Pierre Huneau <pierrehuneau@Hiruma.local>
2011-02-16 19:25:41 +0100

    nothing important

--- src/tom/engine/prettyprinter/ppeditor/src/test/AllTests.java
+++ src/tom/engine/prettyprinter/ppeditor/src/test/AllTests.java
@@ -1,27 +1,14 @@
 package ppeditor.test;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
-public class AllTests {
+@RunWith(Suite.class)
+@SuiteClasses(value={
+TestPPCursor.class,
+TestPPTextPosition.class
+})
 
+public class AllTests {}
 
-public static Test suite() {
-
-TestSuite suite =
-
-new TestSuite("Suite de test pour les outils de calcul");
-
-//$JUnit-BEGIN$
-
-suite.addTest(new TestSuite(TestPPCursor.class));
-
-suite.addTest(new TestSuite(TestPPTextPosition.class));
-
-//$JUnit-END$
-
-return suite;
-
-}
-
-} 	
