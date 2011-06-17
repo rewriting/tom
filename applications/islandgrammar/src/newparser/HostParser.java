2658e986eb6df08d44e032d401bb0392c450895e
greygjhart <gregoire.brenon@mines.inpl-nancy.fr>
2011-06-17 19:29:57 +0200

    [islangrammar] Fully commented HostParser and Keyword + adding test script

--- applications/islandgrammar/src/newparser/HostParser.java
+++ applications/islandgrammar/src/newparser/HostParser.java
@@ -3,12 +3,16 @@ package newparser;
 import org.antlr.runtime.tree.*;
 import org.antlr.runtime.*;
 
+/** <p>This class aims at switching between the various parsers by looking for keywords.</p>
+<p>The HostParser can be used in several ways depending on when you want the parsing to stop. In fact, there are three constructors. You can have HostParser stop when reaching the EOF. You can alternatively set a custom 'EOF' (second constructor). You can also use HostParser and start counting the levels of each block by using an OpenToken and CloseToken.</p>*/
+
 public class HostParser {
 
 /*
  * Definition of the keyword classes used in the rest
  */
 
+/** This is a special Keyword to watch something but do nothing. It is useful to count pattern levels (see DoubleKeyword).*/
   public class Watcher extends Keyword {
     public Watcher(String pattern) {
       this.pattern = pattern;
@@ -17,6 +21,7 @@ public class HostParser {
     public void action() {}
   }
 
+/** This keyword doesn't trigger any parser but instead make the HostParser stop.*/
   public class ExitKeyword extends Keyword {
     public ExitKeyword() {
       this.pattern = String.valueOf((char) CharStream.EOF);
@@ -31,6 +36,7 @@ public class HostParser {
     }
   }
 
+/** The 'match' keyword to look for Tom's %match.*/
   public class MatchConstruct extends Keyword {
     public MatchConstruct() {
       this.pattern = "%match";
@@ -44,6 +50,7 @@ public class HostParser {
     }
   }
 
+/** The 'typeterm' keyword to look for Tom's %typeterm.*/
   public class TypeTermConstruct extends Keyword {
     public TypeTermConstruct() {
       this.pattern = "%typeterm";
@@ -57,6 +64,7 @@ public class HostParser {
     }
   }
 
+/** A special keyword that 'eats' the inside of a comment to make it disappear from the host content (preventing HostParser to see commented constructs like %match).*/
   public class Comment extends Keyword {
     public Comment() {
       this.pattern = "/*";
@@ -71,15 +79,10 @@ public class HostParser {
         input.consume();
       }
       input.consume();
-/*      packHostContent();
-      CommentLexer lexer = new CommentLexer(input);
-      CommonTokenStream tokens = new CommonTokenStream(lexer);
-      CommentParser parser = new CommentParser(tokens);
-      parser.prog();
-//      arbre.addChild((Tree) parser.regular().getTree());*/
     }
   }
 
+/** The same as Comment, but acting on one line comments (thus skipping line like "// %typeterm").*/
   public class OLComment extends Keyword {
     public OLComment() {
       this.pattern = "//";
@@ -90,15 +93,10 @@ public class HostParser {
         input.consume();
       }
       input.consume();
-/*      packHostContent();
-      CommentLexer lexer = new CommentLexer(input);
-      CommonTokenStream tokens = new CommonTokenStream(lexer);
-      CommentParser parser = new CommentParser(tokens);
-      parser.prog();
-//      arbre.addChild((Tree) parser.oneline().getTree());*/
     }
   }
 
+/** This class extends the mechanism provided by the comment Keywords to the host content between double quotes, like what is interpreted as strings in Java and most of languages (so that writing "%match" inside the host content - with the characters " - won't trigger the MatchConstruct keyword).*/
   public class HostString extends Keyword {
     public HostString() {
       this.pattern = "\"";
@@ -113,6 +111,7 @@ public class HostParser {
     }
   }
 
+/** Same as HostString, but with the simple quotes.*/
   public class HostCharacter extends Keyword {
     public HostCharacter() {
       this.pattern = "'";
@@ -127,7 +126,7 @@ public class HostParser {
     }
   }
 
-/* Next one is a tool to use two Keywords at the same time with a 'level' to count the number of { and } for instance (but it can be used with any pattern you like) */
+/** This Keyword is a tool to use two Keywords at the same time with a 'level' to count the number of { and } for instance (but it can be used with any pattern you like).*/
   public class DoubleKeyword extends Keyword {
     private Keyword openKeyword;
     private Keyword closeKeyword;
@@ -164,31 +163,30 @@ public class HostParser {
 ** Fields definition
 */
 
+/** The input file that going to be parsed by the program.*/
   private CharStream input;
-
-  private boolean alive;/* whether the getTree() routine should go on or not */
-
-  private StringBuffer savedContent;/* a memory to store characters read before asserting whether they're host content or not */
-  private StringBuffer hostContent;/* the characters that remain uninterpreted */
-  
+/** A flag to control the getTree while loop - set to false to end the parsing.*/
+  private boolean alive;/* whether the getTree() routine should go on or not.*/
+/** A buffer to store characters before deciding whether they're host content or not.*/
+  private StringBuffer savedContent;
+/** A buffer to store characters that aren't dealt with (the 'host' content).*/
+  private StringBuffer hostContent;
+/** The tree that we'll be returning to represent the file once the parsing is over.*/
   private Tree arbre;
-  private Keyword[] keywords;/* This array will contain the keywords that trigger various events */
+/** This array will contain the Keywords to look for.*/
+  private Keyword[] keywords;
 
-/*
-** Constructors
+
+/** Default constructor to create a HostParser stopping when it finds EOF.
+    @param input a CharStream to read the data from
 */
-
-  /* default one : HostParser stops at the end of file */
   public HostParser(CharStream input) {
     this.input = input;
-
-    alive = true;
-
-    savedContent = new StringBuffer();
-    hostContent = new StringBuffer();
-
-    arbre = new CommonTree(new CommonToken(1,"HostBlock"));
-    keywords = new Keyword[] {
+    this.alive = true;
+    this.savedContent = new StringBuffer();
+    this.hostContent = new StringBuffer();
+    this.arbre = new CommonTree(new CommonToken(1,"HostBlock"));
+    this.keywords = new Keyword[] {
       (new ExitKeyword()),
       (new MatchConstruct()),
       (new TypeTermConstruct()),
@@ -199,51 +197,60 @@ public class HostParser {
     };
   }
 
-  /* another one : HostParser stops at StopToken */
+/** Stops when encountering an arbitrary keyword.
+    @param input a CharStream to read the data from
+    @param StopToken the keyword that will act as a EOF for this instance of HostParser
+*/
   public HostParser(CharStream input, String StopToken) {
     this(input);
     keywords[0] = new ExitKeyword(StopToken);
   }
 
-  public HostParser(CharStream input, String OpenToken, String CloseToken) {
+/** Stops when encountering an arbitrary keyword at imbrication level 0. Each time OpenToken is encountered the level is incremented, and decremented when CloseToken  is found. This acts as parenthesis matching system.
+    @param input a CharStream to read the data from
+    @param OpenToken the keyword that will act as an open bracket
+    @param StopToken the keyword that will act as a close bracket
+*/
+  public HostParser(CharStream input, String OpenToken, String StopToken) {
     this(input);
-    keywords[0] = new DoubleKeyword(new Watcher(OpenToken), new ExitKeyword(CloseToken));
+    keywords[0] = new DoubleKeyword(new Watcher(OpenToken), new ExitKeyword(StopToken));
   }
 
-/*
-** Main routine : read the input and produce corresponding Tree
+/** getTree() is the 'main' function of this class : it reads the input and produces the corresponding Tree.
+    @return an ANTLR Tree representing the program described in the input.
 */
   public Tree getTree() {
     while (alive) {
       char read = (char) input.LA(1);
       boolean found = false;
-//      System.out.println(read);
       for(Keyword k : keywords) {
-/* Warning ! Logic is lazy in Java ! had we written 'found || keywords…', keywords left wouldn't be fed 'read' as soon as one of them was ok */
+/* Warning ! Logic is lazy in Java ! had we written 'found || keywords…', keywords left wouldn't be fed 'read' as soon as one of them was ok with it.*/
         found =  k.take(read) || found;
       }
       savedContent.append(read);
       input.consume();
       if(!found) {
+/* No keyword has accepted the character 'read'. This means we are no longer recognizing a patten, so what we've found so far is actually host content, and should be stored with the rest in the hostContent.*/
         hostContent.append(savedContent);
-        savedContent.setLength(0);
+        savedContent.setLength(0); /* 'empty' the savedContent buffer by setting its length to 0.*/
       }
-      for(Keyword k : keywords) {
-        if(k.isReady()) {
-          try {
-            k.action();
-            k.reset();
-          } catch (Exception e) {e.printStackTrace();}
+      else {
+/* A keyword can be ready only if it has accepted last character read, so this is the  case only if 'found'.*/
+        for(Keyword k : keywords) {
+          if(k.isReady()) {
+/* This test should fail for all but at most one keyword. Otherwise, it means that we've triggered several keywords at the same time. This is probably unwanted, as the action triggered will most likely modify the input stream, meaning that the other keyword's action will take place on an unknown context. This will be the case for instance if you have to keywords watching respectively 'abc' and 'bc'. To allow such things, it would be good to allow reg. exp. based patterns (and 'bc' would then become '[^a]bc' and wouldn't conflict with 'abc' anymore).*/
+            try {
+              k.action();
+              k.reset();
+            } catch (Exception e) {e.printStackTrace();}
+          }
         }
       }
     }
     return arbre;
   }
 
-/*
-** Useful routines called from Keyword classes
-*/
-
+/** A private function, consists in storing the hostContent String on the tree “arbre” as a leaf, by converting it to a Token.*/
   private void packHostContent() {
     savedContent.setLength(0);
     if(hostContent.length() > 0) {
--- applications/islandgrammar/src/newparser/Keyword.java
+++ applications/islandgrammar/src/newparser/Keyword.java
@@ -2,18 +2,20 @@ package newparser;
 
 import org.antlr.runtime.tree.*;
 
+/**<p>Keyword is an abstract class to allow watching for a keyword and trigger an arbitrary action when it is found in the data stream.</p>
+<p> To create a Keyword, you need to define a class inheriting from this one, and to write a proper action() method. Pay attention to the fact that all your keywords' patterns should be different and none should be a substring of another one as it would pose the problem to decide which one should be triggered. Currently it will be accepted by the compiler buruntime behaviour isn't guaranteed.</p>*/
 public abstract class Keyword {
 
-/** The pattern this Keyword looks for */
+/** The pattern this Keyword looks for.*/
   protected String pattern; /* this is protected because it's supposed to be modified by all inheriting classes (the actual Keyword ) */
-/** Index of next character to compare */
+/** Index of next character to compare.*/
   private int cursor = 0; /* this is private because we'll surely want to change the recognition mechanism one day (to introduce reg. exp. based Keyword ?) and don't want to break existing code */
-/** A flag to tell whether or not the pattern has been found */
+/** A flag to tell whether or not the pattern has been found.*/
   protected boolean matched; /* in this implementation it will equal (cursor == pattern.length), but what about more complex recognition mechanism ? (reg. exp. for instance) */
 
 /**
- ** @param  : a character proposed to this Keyword for recognition
- ** @return : whether c was accepted by this Keyword
+ ** @param  c a character proposed to this Keyword to match it's pattern
+ ** @return whether c was accepted by this Keyword
  **/
   public boolean take(char c) {
     if (pattern.charAt(cursor) == c) {
@@ -28,18 +30,18 @@ public abstract class Keyword {
     }
   }
 
-/** A simple getter on boolean 'matched' */
+/** A simple getter on boolean 'matched'.*/
   public boolean isReady() {
     return matched;
   }
 
-/** Puts the Keyword in it's original state */
+/** Puts the Keyword in it's original state.*/
   public void reset() {
     cursor = 0;
     matched = false;
   }
 
-/** The action the Keyword is supposed to do when its pattern is matched */
+/** The action the Keyword is supposed to do when its pattern is matched.*/
   protected abstract void action() throws org.antlr.runtime.RecognitionException;
 
 }
