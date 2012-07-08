6b64150ca7c7447beb588e236bd8f3a4450f24c1
hubert <hubert.godfroy@gmail.com>
2012-07-08 16:51:49 +0200

    New test cases for dstToLeaf()

--- applications/quickcheck/test/definitions/AlgebraicTest.java
+++ applications/quickcheck/test/definitions/AlgebraicTest.java
@@ -5,7 +5,6 @@
 package definitions;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import org.junit.*;
 
 /**
@@ -21,6 +20,7 @@ public class AlgebraicTest {
   Algebraic forest;
   Algebraic circ;
   Algebraic typetest;
+  Algebraic a, b, c;
 
   public AlgebraicTest() {
     scope = new Scope();
@@ -51,10 +51,22 @@ public class AlgebraicTest {
     forest.addConstructor(cons_forest1);
     tree2.addConstructor(cons_tree1);
     tree2.addConstructor(cons_tree2);
-    
+
     circ = new Algebraic(scope);
     Algebraic[] cons_circ = {circ};
     circ.addConstructor(cons_circ);
+
+    a = new Algebraic(scope);
+    b = new Algebraic(scope);
+    c = new Algebraic(scope);
+    Algebraic[] cons_a1 = {};
+    Algebraic[] cons_a2 = {b};
+    Algebraic[] cons_b = {c};
+    Algebraic[] cons_c = {a};
+    a.addConstructor(cons_a1);
+    a.addConstructor(cons_a2);
+    b.addConstructor(cons_b);
+    c.addConstructor(cons_c);
 
     scope.setDependances();
   }
@@ -81,7 +93,6 @@ public class AlgebraicTest {
   @Test
   public void testGetDimentionNat() {
     System.out.println("getDimention Nat");
-
     int expResult = 1;
     int result = nat.getDimention();
     assertEquals(expResult, result);
@@ -90,7 +101,6 @@ public class AlgebraicTest {
   @Test
   public void testGetDimentionTree() {
     System.out.println("getDimention tree");
-
     int expResult = 1;
     int result = tree.getDimention();
     assertEquals(expResult, result);
@@ -99,16 +109,14 @@ public class AlgebraicTest {
   @Test
   public void testGetDimentionTypeTest() {
     System.out.println("getDimention TypeTest");
-
     int expResult = 1;
     int result = typetest.getDimention();
     assertEquals(expResult, result);
   }
-  
+
   @Test
   public void testGetDimentionTree2() {
     System.out.println("getDimention Tree2");
-
     int expResult = 2;
     int result = tree2.getDimention();
     assertEquals(expResult, result);
@@ -117,20 +125,41 @@ public class AlgebraicTest {
   @Test
   public void testGetDimentionForest() {
     System.out.println("getDimention Forest");
-
     int expResult = 2;
     int result = forest.getDimention();
     assertEquals(expResult, result);
-
     assertEquals(2, tree2.getDimention());
   }
 
   @Test
   public void testGetDimentionCirc() {
     System.out.println("getDimention Circ");
-    
     int expResult = 1;
     int result = circ.getDimention();
+    assertEquals(expResult, result);
+  }
+
+  @Test
+  public void testGetDimentionA() {
+    System.out.println("getDimention A");
+    int expResult = 1;
+    int result = a.getDimention();
+    assertEquals(expResult, result);
+  }
+
+  @Test
+  public void testGetDimentionB() {
+    System.out.println("getDimention B");
+    int expResult = 1;
+    int result = b.getDimention();
+    assertEquals(expResult, result);
+  }
+
+  @Test
+  public void testGetDimentionC() {
+    System.out.println("getDimention C");
+    int expResult = 1;
+    int result = c.getDimention();
     assertEquals(expResult, result);
   }
 
@@ -139,7 +168,6 @@ public class AlgebraicTest {
    */
   @Test
   public void testDstToLeafNat() {
-
     System.out.println("dstToLeaf Nat");
     int expResult = 0;
     int result = nat.dstToLeaf();
@@ -148,7 +176,6 @@ public class AlgebraicTest {
 
   @Test
   public void testDstToLeafTree() {
-
     System.out.println("dstToLeaf Tree");
     int expResult = 0;
     int result = tree.dstToLeaf();
@@ -157,7 +184,6 @@ public class AlgebraicTest {
 
   @Test
   public void testDstToLeafTypeTest() {
-
     System.out.println("dstToLeaf TypeTest");
     int expResult = 1;
     int result = typetest.dstToLeaf();
@@ -166,7 +192,6 @@ public class AlgebraicTest {
 
   @Test
   public void testDstToLeafForest() {
-
     System.out.println("dstToLeaf Forest");
     int expResult = 2;
     int result = forest.dstToLeaf();
@@ -175,7 +200,6 @@ public class AlgebraicTest {
 
   @Test
   public void testDstToLeafTree2() {
-
     System.out.println("dstToLeaf Tree2");
     int expResult = 1;
     int result = tree2.dstToLeaf();
@@ -184,10 +208,33 @@ public class AlgebraicTest {
 
   @Test
   public void testDstToLeafCirc() {
-
     System.out.println("dstToLeaf Circ");
     int expResult = Integer.MAX_VALUE;
     int result = circ.dstToLeaf();
+    assertEquals(expResult, result);
+  }
+
+  @Test
+  public void testDstToLeafB() {
+    System.out.println("dstToLeaf B");
+    int expResult = 2;
+    int result = b.dstToLeaf();
+    assertEquals(expResult, result);
+  }
+
+  @Test
+  public void testDstToLeafA() {
+    System.out.println("dstToLeaf A");
+    int expResult = 0;
+    int result = a.dstToLeaf();
+    assertEquals(expResult, result);
+  }
+
+  @Test
+  public void testDstToLeafC() {
+    System.out.println("dstToLeaf C");
+    int expResult = 1;
+    int result = c.dstToLeaf();
     assertEquals(expResult, result);
   }
 }
