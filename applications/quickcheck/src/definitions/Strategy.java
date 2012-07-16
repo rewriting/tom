d63f0a116fe9776613f89b98e6e816db45e2faf9
hubert <hubert.godfroy@gmail.com>
2012-07-16 17:45:41 +0200

    add comment

--- applications/quickcheck/src/definitions/Strategy.java
+++ applications/quickcheck/src/definitions/Strategy.java
@@ -10,7 +10,16 @@ import java.util.HashSet;
  *
  * @author hubert
  */
- interface Strategy {
-  
-   HashSet<Slot> fillATerm(Slot aTerm, int n);
+interface Strategy {
+
+  /**
+   * Fill a slot by choosing a contructor for it. This choice is influenced by
+   * the type of the strategy. According to Agata algorithm, it must also fill
+   * all subterm with egal dimention of this term.
+   *
+   * @param aTerm
+   * @param n
+   * @return Set of fields of the filled terms
+   */
+  HashSet<Slot> fillATerm(Slot aTerm, int n);
 }
