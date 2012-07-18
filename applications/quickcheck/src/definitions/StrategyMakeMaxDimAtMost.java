36fec48d52c88b50f409396f539b9b77b5d1eac3
hubert <hubert.godfroy@gmail.com>
2012-07-18 14:03:10 +0200

    begin implementing AtMost strategy

--- applications/quickcheck/src/definitions/StrategyMakeMaxDimAtMost.java
+++ applications/quickcheck/src/definitions/StrategyMakeMaxDimAtMost.java
@@ -10,11 +10,43 @@ import java.util.HashSet;
  *
  * @author hubert
  */
-public class StrategyMakeMaxDimAtMost implements Strategy{
+class StrategyMakeMaxDimAtMost implements Strategy {
 
   @Override
-  public HashSet<Slot> fillATerm(Slot aTerm, int n, int distStrategy) {
-    throw new UnsupportedOperationException("Not supported yet.");
+  public HashSet<Slot> fillATerm(Slot aTerm, int ni, int distStrategy) {
+    HashSet<Slot> res = new HashSet<Slot>();
+
+    //fill the term by choosing one of its constructors
+    Slot[] fields = aTerm.chooseMaxDimConstructor();
+
+    //dispatch fields of the term between two categories: these whose dimension 
+    //equals dimension of the term, and the others
+    HashSet<Slot> listHigherDimFields = new HashSet<Slot>();
+    for (int i = 0; i < fields.length; i++) {
+      Slot field = fields[i];
+      if (field.getDimension() < aTerm.getDimension()) {
+        res.add(field);
+      } else {
+        listHigherDimFields.add(field);
+      }
+    }
+
+    //spread number of recursions of the curent term into each fields with the 
+    //same dimension
+    int[] nis = Random.pile(ni - 1, listHigherDimFields.size());
+
+    //re-apply algorithm on same dimension fields in order to eliminate them
+    int i = 0;
+    for (Slot field : listHigherDimFields) {
+      Strategy req;
+      if (field.distToLeaf(distStrategy) < ni) {
+        req = new StrategyMakeMaxDimAtMost();
+      } else {
+        req = new StrategyBacktrackDepth();
+      }
+      res.addAll(req.fillATerm(field, nis[i], distStrategy));
+      i++;
+    }
+    return res;
   }
-  
 }
