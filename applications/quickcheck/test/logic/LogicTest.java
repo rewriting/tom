6686d73a353257cd083097d4cacb4f71302d13fb
hubert <hubert.godfroy@gmail.com>
2012-07-27 10:12:03 +0200

    add tests on lists

--- applications/quickcheck/test/logic/LogicTest.java
+++ applications/quickcheck/test/logic/LogicTest.java
@@ -7,6 +7,8 @@ package logic;
 import aterm.ATerm;
 import aterm.ATermInt;
 import aterm.pure.PureFactory;
+import definitions.Scope;
+import definitions.SortGom;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,7 +17,12 @@ import logic.model.PredicateInterpretation;
 import logic.model.SignatureInterpretation;
 import gen.ExamplesFormula; // generated
 import gen.Interpretation; // generated
+import gen.TestGen;
 import org.junit.*;
+import sort.types.IntList;
+import sort.types.Leaf;
+import sort.types.intlist.consList;
+import sort.types.intlist.nill;
 import system.types.Formula; // generated
 
 /**
@@ -25,12 +32,17 @@ import system.types.Formula; // generated
 public class LogicTest {
 
   private Formula formula = ExamplesFormula.f1;
+  // Interger interpretation
   private Interpretation interp_int;
   private PredicateInterpretation P_int;
   private DomainInterpretation D_int;
+  // List interpretation
+  private Interpretation interp_list;
+  private PredicateInterpretation P_list;
+  private DomainInterpretation D_list;
 
   public LogicTest() {
-    
+
     P_int = new PredicateInterpretation() {
 
       @Override
@@ -40,23 +52,61 @@ public class LogicTest {
       }
     };
 
+    P_list = new PredicateInterpretation() {
+
+      @Override
+      public boolean isTrue(List<ATerm> args) {
+        IntList list = IntList.fromTerm(args.get(0));
+        return list.equals(TestGen.reverse(TestGen.reverse(list)));
+      }
+    };
+
     D_int = new DomainInterpretation() {
 
       @Override
       public ATerm chooseElement() {
         PureFactory factory = new PureFactory();
-        return factory.makeInt((int) (Math.random()*1000));
+        return factory.makeInt((int) (Math.random() * 1000));
+      }
+    };
+    D_list = new DomainInterpretation() {
+
+      @Override
+      public ATerm chooseElement() {
+
+        Scope scope = new Scope();
+
+        SortGom leaf = new SortGom(scope, Leaf.class);
+        SortGom list = new SortGom(scope, IntList.class);
+        list.addConstructor(nill.class);
+        list.addConstructor(consList.class);
+        leaf.addConstructor("a");
+        leaf.addConstructor("b");
+        leaf.addConstructor("c");
+
+        scope.setDependances();
+        
+        ATerm res = list.generate(20);
+        return res;
       }
     };
 
-    Map<String, PredicateInterpretation> map_pre = new HashMap<String, PredicateInterpretation>();
-    Map<String, SignatureInterpretation> map_sig = new HashMap<String, SignatureInterpretation>();
-    Map<String, DomainInterpretation> map_dom = new HashMap<String, DomainInterpretation>();
+    Map<String, PredicateInterpretation> map_pre_int = new HashMap<String, PredicateInterpretation>();
+    Map<String, SignatureInterpretation> map_sig_int = new HashMap<String, SignatureInterpretation>();
+    Map<String, DomainInterpretation> map_dom_int = new HashMap<String, DomainInterpretation>();
+    
+    Map<String, PredicateInterpretation> map_pre_list = new HashMap<String, PredicateInterpretation>();
+    Map<String, SignatureInterpretation> map_sig_list = new HashMap<String, SignatureInterpretation>();
+    Map<String, DomainInterpretation> map_dom_list = new HashMap<String, DomainInterpretation>();
 
-    map_pre.put("P", P_int);
-    map_dom.put("D", D_int);
+    map_pre_int.put("P", P_int);
+    map_dom_int.put("D", D_int);
+    
+    map_pre_list.put("P", P_list);
+    map_dom_list.put("D", D_list);
 
-    interp_int = new Interpretation(map_pre, map_sig, map_dom);
+    interp_int = new Interpretation(map_pre_int, map_sig_int, map_dom_int);
+    interp_list = new Interpretation(map_pre_list, map_sig_list, map_dom_list);
   }
 
   @BeforeClass
@@ -79,10 +129,17 @@ public class LogicTest {
   public void testInterpInt() {
     assert interp_int.validateFormula(formula, new HashMap<String, ATerm>());
   }
+
   @Test
   public void testInterpIntCE() {
     String res = interp_int.validateFormulaWithCE(formula, new HashMap<String, ATerm>()).toString();
     System.out.println(res);
+    assert res.equals("NoCE()");
+  }
+  
+  @Test
+  public void testInterpListCE() {
+    String res = interp_list.validateFormulaWithCE(formula, new HashMap<String, ATerm>()).toString();
     assert res.equals("NoCE()");
   }
 }
