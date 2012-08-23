95bb8cdceebd2a4d1ad14c81e671048606aaa829
hubert <hubert.godfroy@gmail.com>
2012-08-23 11:02:28 +0200

    documentation of ATermIterator

--- applications/quickcheck/src/aterm/ATermIterator.java
+++ applications/quickcheck/src/aterm/ATermIterator.java
@@ -10,15 +10,35 @@ import java.util.logging.Logger;
 import logic.model.ShrinkIterator;
 
 /**
+ * This class makes it possible to iterate over an list of ATerm. This
+ * implementation is different of Iterator's one because Iterator cannot
+ * implements Clonable interface and thus can be used only once.
  *
  * @author hubert
  */
 public abstract class ATermIterator implements Cloneable {
 
+  /**
+   * Returns true if the iteration has more elements. (In other words, returns
+   * true if next would return an element rather than throwing an exception.)
+   *
+   * @return true if the iterator has more elements.
+   */
   abstract public boolean hasNext();
 
+  /**
+   * Returns the next element in the iteration.
+   *
+   * @return the next element in the iteration.
+   */
   abstract public ATerm next();
 
+  /**
+   * Clones an iterator.
+   *
+   * @return a new iterator initialised at the first element of the cloned
+   * iterator
+   */
   @Override
   public ATermIterator clone() {
     try {
@@ -29,6 +49,13 @@ public abstract class ATermIterator implements Cloneable {
     return null;
   }
 
+  /**
+   * Tells if to iterators are equal, that is, iterate overs the same éléments
+   * in the same order.
+   *
+   * @param other iterator to be compared with.
+   * @return true if the iterators are equals
+   */
   public boolean equals(ATermIterator other) {
     ATermIterator c1 = this.clone();
     ATermIterator c2 = other.clone();
@@ -46,9 +73,31 @@ public abstract class ATermIterator implements Cloneable {
       return true;
     }
   }
+  
+  private ATermList toATermList(ATermIterator iterator, ATermFactory factory) {
+    if(!iterator.hasNext()) {
+      return factory.makeList();
+    }
+    ATerm term = iterator.next();
+    return toATermList(iterator, factory).insert(term);
+  }
+  
+  /**
+   * Gives a representation of an iterator in ATermList.
+   * @param factory factory used to build the ATermList.
+   * @return ATermList representation.
+   */
+  public ATermList toATermList(ATermFactory factory) {
+    return toATermList(this.clone(), factory);
+  }
 
+  /**
+   * Gives a representation of an iterator in String.
+   * @return String representation.
+   */
   @Override
   public String toString() {
-    return ShrinkIterator.toATermList(this.clone(), new PureFactory()).toString();
+    return this.toATermList(new PureFactory()).toString();
+//    return ShrinkIterator.toATermList(this.clone(), new PureFactory()).toString();
   }
 }
