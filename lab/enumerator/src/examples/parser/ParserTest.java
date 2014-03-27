90b2e5c0275e282dcde5d304db050211f3d099b0
balland <emilie.balland@gmail.com>
2014-03-27 16:54:05 +0100

    revert ParserTest with the user-defined ExpGenerator

--- lab/enumerator/src/examples/parser/ParserTest.java
+++ lab/enumerator/src/examples/parser/ParserTest.java
@@ -3,10 +3,14 @@ package examples.parser;
 import org.junit.contrib.theories.Theory;
 import org.junit.runner.RunWith;
 
+import com.pholser.junit.quickcheck.ForAll;
+import com.pholser.junit.quickcheck.From;
+
 import tom.library.enumerator.Enumeration;
 import tom.library.theory.Enum;
 import tom.library.theory.RandomForAll;
 import tom.library.theory.TomCheck;
+import examples.junit.quickcheck.ExpGenerator;
 import examples.parser.rec.types.Exp;
 
 @RunWith(TomCheck.class)
@@ -15,11 +19,15 @@ public class ParserTest {
 	@Enum public static Enumeration<Exp> enumeration = Exp.getEnumeration();
 
 	@Theory
-	public void testExp(@RandomForAll(sampleSize=100) Exp n) {
+	public void testExp(@ForAll(sampleSize=100) @From({ ExpGenerator.class }) Exp n) {
 		System.out.println("Quick: "+n);
 	}
 
-
+	@Theory
+	public void testExp2(@RandomForAll(sampleSize=100) Exp n) {
+		System.out.println("Quick: "+n);
+	}
+	
 
 
 }
