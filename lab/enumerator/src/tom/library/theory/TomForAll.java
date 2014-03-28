a2c6bc9d2181a49bcf83962f431d37ab7bce3eb9
Horatiu Cirstea <cirstea@tupai-2.home>
2014-03-28 09:31:43 +0100

    supplier with tom enumerations - chantier

---
+++ lab/enumerator/src/tom/library/theory/TomForAll.java
@@ -0,0 +1,30 @@
+package tom.library.theory;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.junit.contrib.theories.ParametersSuppliedBy;
+
+
+// CHANTIER
+public class TomForAll {
+
+	
+	/**
+	 * Mark a parameter of a {@link org.junit.contrib.theories.Theory Theory} method with this annotation to have
+	 * generated values supplied to it exhaustively as in SmallCheck.
+	 */
+
+	@ParametersSuppliedBy(TomValueSupplier.class)
+	@Target(PARAMETER)
+	@Retention(RUNTIME)
+
+	public @interface ExhaustiveForAll {
+
+		public int 	maxDepth() default 100;
+		
+	}
+}
---
+++ lab/enumerator/src/tom/library/theory/TomValueSupplier.java
@@ -0,0 +1,66 @@
+package tom.library.theory;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.contrib.theories.ParameterSignature;
+import org.junit.contrib.theories.ParameterSupplier;
+import org.junit.contrib.theories.PotentialAssignment;
+
+import tom.library.enumerator.Enumeration;
+import tom.library.enumerator.Finite;
+import tom.library.enumerator.LazyList;
+
+
+//CHANTIER
+public class TomValueSupplier extends ParameterSupplier {
+
+	private BigInteger nextRandomBigInteger(BigInteger n) {
+		Random rand = new Random();
+		BigInteger result = new BigInteger(n.bitLength(), rand);
+		while (result.compareTo(n) >= 0) {
+			result = new BigInteger(n.bitLength(), rand);
+		}
+		return result;
+	}
+
+	@Override
+	public List<PotentialAssignment> getValueSources(ParameterSignature signature) {
+		
+		int samplesize = signature.getAnnotation(RandomForAll.class)
+				.sampleSize();
+		final Enumeration<?> enumeration = TomCheck.get(signature.getType());
+		List<PotentialAssignment> l = new ArrayList<PotentialAssignment>();
+		LazyList<?> parts = enumeration.parts();
+		int i = 0;
+		while (i < samplesize) {
+			final Finite<?> part = (Finite<?>) parts.head();
+			if (part == null)
+				break;
+			parts = parts.tail();
+			BigInteger card = part.getCard();
+			if (!card.equals(BigInteger.ZERO)) {
+				i++;
+				final BigInteger j = nextRandomBigInteger(card);
+				PotentialAssignment assignment = new PotentialAssignment() {
+
+					@Override
+					public Object getValue()
+							throws CouldNotGenerateValueException {
+						return part.get(j);
+					}
+
+					@Override
+					public String getDescription()
+							throws CouldNotGenerateValueException {
+						return null;
+					}
+				};
+				l.add(assignment);
+			}
+		}
+		return l;
+	}
+}
