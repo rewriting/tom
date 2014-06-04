89a2abdc13ba8560d29c06c3706b622413c260fd
npatmaja <noval.78@gmail.com>
2014-06-04 14:16:09 +0200

    create factory for shop

---
+++ lab/enumerator/src/examples/shop/ShopFactory.java
@@ -0,0 +1,59 @@
+package examples.shop;
+
+public class ShopFactory {
+	private static ShopFactory INSTANCE;
+
+	public static ShopFactory getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new ShopFactory();
+		}
+		return INSTANCE;
+	}
+	
+	public Shop makeShop(examples.shop.shop.types.Inventory inventoryGom) {
+		Inventory inventory = new Inventory();
+		buildInventory(inventoryGom, inventory);
+		Shop shop = new Shop();
+		shop.setInventory(inventory);
+		return shop;
+	}
+	
+	public Inventory makeInventory(examples.shop.shop.types.Inventory inventoryGom) {
+		Inventory inventory = new Inventory();
+		buildInventory(inventoryGom, inventory);
+		return inventory;
+	}
+	
+	private void buildInventory(examples.shop.shop.types.Inventory inventoryGom, Inventory inventory) {
+		if (inventoryGom.isInv()) {
+			inventory.add(makeLineItem(inventoryGom.getLineItem()));
+			examples.shop.shop.types.Inventory tail = inventoryGom.getInventory();
+			buildInventory(tail, inventory);
+		}
+	}
+	
+	public Cart makeCart(examples.shop.shop.types.ShoppingCart cartGom) {
+		Cart cart = new Cart();
+		buildCart(cartGom, cart);
+		return cart;
+	}
+	
+	private void buildCart(examples.shop.shop.types.ShoppingCart cartGom, Cart cart) {
+		if (cartGom.isCart()) {
+			cart.addToCart(makeLineItem(cartGom.getLineItem()));
+			examples.shop.shop.types.ShoppingCart tail = cartGom.getShoppingCart();
+			buildCart(tail, cart);
+		}
+	}
+	
+	public LineItem makeLineItem(examples.shop.shop.types.LineItem lItemGom) {
+		int quantity = Math.abs(lItemGom.getQuantity());
+		return new LineItem(makeItem(lItemGom.getItem()), quantity);
+	}
+	
+	public Item makeItem(examples.shop.shop.types.Item itemGom) {
+		int id = Math.abs(itemGom.getId());
+		int price = Math.abs(itemGom.getPrice());
+		return new Item(id, price);
+	}
+}
