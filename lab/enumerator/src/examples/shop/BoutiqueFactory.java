ead09577fff8f45f7b0fa2014895c576e8ea9f6d
Horatiu Cirstea <cirstea@morea.loria.fr>
2014-06-05 17:21:52 +0200

    start merging shop factories

--- lab/enumerator/src/examples/shop/BoutiqueFactory.java
+++ lab/enumerator/src/examples/shop/BoutiqueFactory.java
@@ -12,13 +12,29 @@ public class BoutiqueFactory {
 		return INSTANCE;
 	}
 	
-	public Item makeItem(examples.shop.boutique.types.Item boutiqueItem) {
-		return new Item(boutiqueItem.getid(), boutiqueItem.getprice());
+	// take the absolute value to avoid negative ids/prices
+	// this means that we suppose that all items are well formed
+	// there should be some tests with items built out of possibly negative 
+	// integers so we van test the behavior in these cases
+	public Item makeItem(examples.shop.boutique.types.Item item) {
+		return new Item(Math.abs(item.getid()), Math.abs(item.getprice()));
+	}
+
+	public Item makeItem(examples.shop.shop.types.Item item) {
+		return new Item(Math.abs(item.getId()), Math.abs(item.getPrice()));
+	}
+
+	// take the absolute value to avoid quantities
+	// could be generated automatically from the class LineItem
+	public LineItem makeLineItem(examples.shop.boutique.types.LineItem lineItem) {
+		return new LineItem(makeItem(lineItem.getitem()), Math.abs(lineItem.getquantity()));
+	}
+
+	public LineItem makeLineItem(examples.shop.shop.types.LineItem lineItem) {
+		return new LineItem(makeItem(lineItem.getItem()), Math.abs(lineItem.getQuantity()));
 	}
 	
-	public LineItem makeLineItem(examples.shop.boutique.types.LineItem boutiqueLineItem) {
-		return new LineItem(makeItem(boutiqueLineItem.getitem()), boutiqueLineItem.getquantity());
-	}
+	
 	
 	public Inventory makeInventory(examples.shop.boutique.types.Inventory boutiqueInventory) {
 		Inventory inventory = new Inventory();
