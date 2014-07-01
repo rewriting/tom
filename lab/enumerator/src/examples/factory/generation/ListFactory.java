169511c3bed8e07b46bdc20abbcd9cf2e0b1b556
Yann <yann.hortance7@etu.univ-lorraine.fr>
2014-07-01 15:44:20 +0200

    add ListFactory

---
+++ lab/enumerator/src/examples/factory/generation/ListFactory.java
@@ -0,0 +1,14 @@
+package examples.factory.generation;
+
+import java.io.IOException;
+
+public class ListFactory extends AbstractGeneratorFactory{
+
+	@Override
+	protected StringBuilder core(FieldConstructor fc, String packagePath)
+			throws IOException {
+		// TODO Auto-generated method stub
+		return new StringBuilder();
+	}
+
+}
