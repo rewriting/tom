b86370671a35faaba7969a89bfe923da18481698
Ahmad Abdelghany <ahmad-ibrahim.abdelghany-mohamed@loria.fr>
2015-04-02 17:14:47 +0200

    add getters to Student

--- lab/enumerator/src/examples/factory/Student.java
+++ lab/enumerator/src/examples/factory/Student.java
@@ -1,26 +1,33 @@
 package examples.factory;
 
 import java.util.ArrayList;
-
 import tom.library.factory.Enumerate;
 import tom.library.factory.EnumerateGenerator;
 
 public class Student {
 
-	private int no;
-	private String name;
+    private int no;
+    private String name;
 
-	@EnumerateGenerator(canBeNull = true)
-	public Student(@Enumerate(maxSize = 8) int no,
-			@Enumerate(maxSize = 4) String string) {
-		this.no = no;
-		this.name = string;
-		new ArrayList<Integer>();
-	}
+    @EnumerateGenerator(canBeNull = true)
+    public Student(@Enumerate(maxSize = 8) int no,
+        @Enumerate(maxSize = 4) String string) {
+        this.no = no;
+        this.name = string;
+        new ArrayList<Integer>();
+    }
 
-	@Override
-	public String toString() {
-		return "Student [no=" + no + ", name=" + name + "]";
-	}
+    public int getNo() {
+        return no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [no=" + no + ", name=" + name + "]";
+    }
 
 }
