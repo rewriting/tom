2c1ec7f6953070ab7c88c8fdac05437618a84dd7
Ahmad Abdelghany <ahmad-ibrahim.abdelghany-mohamed@loria.fr>
2015-04-08 11:47:06 +0200

    adding bidirectional one to one relation example

---
+++ lab/enumerator/src/examples/factory/StudentCar.java
@@ -0,0 +1,23 @@
+package examples.factory;
+
+import tom.library.factory.Enumerate;
+import tom.library.factory.EnumerateGenerator;
+
+public class StudentCar {
+    
+    private int no;
+    private StudentWithCar driver;
+
+
+    @EnumerateGenerator(canBeNull = true)
+    public StudentCar(@Enumerate(maxSize = 8) int no, @Enumerate(maxSize = 4) StudentWithCar driver) {
+        this.no = no;
+        this.driver = driver;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentCar [no=" + no + ", driver=" + driver + "]";
+    }
+
+}
---
+++ lab/enumerator/src/examples/factory/StudentWithCar.java
@@ -0,0 +1,43 @@
+package examples.factory;
+
+import java.util.ArrayList;
+import tom.library.factory.Enumerate;
+import tom.library.factory.EnumerateGenerator;
+
+public class StudentWithCar {
+
+    private int no;
+    private String name;
+    private StudentCar car;
+
+    @EnumerateGenerator(canBeNull = true)
+    public StudentWithCar(
+        @Enumerate(maxSize = 8) int no,
+        @Enumerate(maxSize = 4) String name,
+        @Enumerate(maxSize = 6) StudentCar car
+    ) {
+        this.no = no;
+        this.name = name;
+        this.car = car;
+        new ArrayList<Integer>();
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    
+    public StudentCar getCar() {
+        return car;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentWithCar [no=" + no + ", name=" + name + ", studentcar="+car+ "]";
+    }
+
+}
