2c71cd0d4de6a30b01672d2febe577d24a67a60f
pem <pierre-etienne.moreau@loria.fr>
2016-10-10 18:16:27 +0200

    ready to remove xml and bootstrap with newparser

--- src/tom/library/xml/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Provides the Tom XML library.
- * <p>
- * This package provides tools to convert back and forth between DOM XML
- * representation and TNode (a Gom structure for XML).
- *
- * @see tom.library.adt.tnode.types.TNode
- */
-package tom.library.xml;
