95c72233ef88c2ee08a9cfff20fdf63300704d24
Jean-Christophe Bach <jc.bach@telecom-bretagne.eu>
2017-01-12 17:39:47 +0100

    improve the script

--- changeCopyright.sh
+++ changeCopyright.sh
@@ -9,7 +9,11 @@ else
   NEWYEAR=`date +%Y`
 fi
 
-DIR="./src/"
+DIR="./src/tom/"
+if [ -n "$2" ] ; then
+  DIR=$2
+fi
 
+#EXCLUDE="--exclude-dir xxx"
 grep -rl "\(2[0-9]\{3\}\)-2[0-9]\{3\}" $DIR | xargs sed -i "s/\(2[0-9]\{3\}\)-2[0-9]\{3\}/\1-$NEWYEAR/g"
 
